10f7d2e356b1e07ab4d491a9a7c2d5d98eb2b18e
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-09-20 19:18:10 +0200

    Finalitzat 00_05

---
+++ 00_05_presentemnos/Empresento.java
@@ -0,0 +1,5 @@
+public class Empresento {
+    public static void main(String[] args) {
+        System.out.println("Hola! Sóc Neus, la futura millor desenvolupadora del món!");
+        }
+}
--- personal/HolaMon.java
+++
@@ -1,5 +0,0 @@
-/* Programa que saluda al món */
-public class HolaMon {
-	public static void main(String[] args {
-		System.out.println("Hola món!");
-	}
