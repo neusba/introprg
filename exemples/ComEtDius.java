7ddfc41eec08187057c2b305789d8fa3e7ff20b2
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-09-21 20:02:26 +0200

    Finalitzat 31_05  ∾ prgtest resultat 31_05: FALLA

---
+++ exemples/ComEtDius.java
@@ -0,0 +1,11 @@
+/* Petit programa que mostra la funcionalitat de la sintaxi Entrada
+ * Sintaxi: Entrada.readLine() 
+ */
+ 
+ public class ComEtDius {
+    public static void main(String[] args) {
+        System.out.println("Com et dius?");
+        String nombre = Entrada.readLine();
+        System.out.println("Hola " + nombre + ", quant de temps!");
+    }
+}
