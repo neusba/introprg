218b6c07ae686b13f7410efcc6014237704ed9a8
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-03 13:50:34 +0200

    Finalitzat 31_17

--- 31_17_ternari/ParellSenar.java
+++ 31_17_ternari/ParellSenar.java
@@ -1,6 +1,6 @@
 /* Programa per saber si un número és parell o senar amb un operador ternari */
 
-public class Ternari {
+public class ParellSenar {
 	public static void main(String[] args) {
 		int numero = Integer.parseInt(args[0]);
 
