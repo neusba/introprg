37eab530fc76b907ee9e5e0be313597076f4da6c
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-10-04 20:28:09 +0200

    Finalitzat 31_22  ∾ prgtest resultat 31_22: FALLA

---
+++ 31_21_diessetmana_switch_exp/DiaSetmanaSwitchExp.java
@@ -0,0 +1,12 @@
+/* Programa que demana per entrada estàndard un número entre l'1 i el 7 i torna per pantalla el día de la setmana corresponent, en aquest cas utilizarem la expressió switch i l'operador ternari. */
+
+public class DiaSetmanaSwitchExp {
+	public static void main(String[] args) {
+	int number = Integer.parseInt(args[0]);
+
+		switch (number)  {
+			case 1: 'Dilluns';
+		};
+	}
+}
+			
