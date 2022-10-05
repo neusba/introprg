0eb314141605c60ae642eae3494585c43153caf1
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-05 20:38:16 +0200

    Finalitzat 31_24

---
+++ 31_24_calculadora_senzilla/CalculadoraSenzilla.java
@@ -0,0 +1,37 @@
+/*
+ * Aquest programa demana dos operands i un operador binari (+, -, * o /)
+ * i mostra el resultat del càlcul corresponent
+ */
+
+public class CalculadoraSenzilla {
+	public static void main(String[] args) {
+		System.out.println("Operand1?");
+		double operand1 = Integer.parseInt(Entrada.readLine());
+		
+		System.out.println("Operador");
+		char operador = Entrada.readLine().charAt(0);
+
+		System.out.println("Operand2?");
+		double operand2 = Integer.parseInt(Entrada.readLine());
+
+		if (operador == '+') {
+			double resultat = operand1 + operand2;
+			System.out.println(operand1 + " + " + operand2 + " = " + resultat);
+		} else if (operador == '-') {
+			double resultat = operand1 - operand2;
+			System.out.println(operand1 + " - " + operand2 + " = " + resultat);
+		} else if (operador == '*') {
+			double resultat = operand1 * operand2;
+			System.out.println(operand1 + " * " + operand2 + " = " + resultat);
+		} else if (operador == '/') {
+			double resultat = operand1 / operand2;
+			if (operand2 == 0) {
+				System.out.println("No es pot dividir entre 0");
+			} else {
+				System.out.println(operand1 + " / " + operand2 + " = " + resultat);
+			}
+		} else {
+			System.out.println("L'operador " + operador + " no està disponible");
+		}
+	}
+}
