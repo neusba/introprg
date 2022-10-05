7c8e95e3ab473a88b61a24cd167546656eeeb410
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-05 22:11:03 +0200

    Finalitzat 31_25

---
+++ 31_25_pagaments/Pagaments.java
@@ -0,0 +1,24 @@
+/* Desenvolupem un programa que simuli una transacció en la que es paga una quantitat de € y depén del que l'usuari doni indica si sobran o si falten diners */
+
+public class Pagaments {
+	public static void main(String[] args) {
+		System.out.println("Preu?");
+		int preu = Integer.parseInt(Entrada.readLine());
+
+		System.out.println("Paga?");
+		int pagament = Integer.parseInt(Entrada.readLine());
+
+		int canvi;
+
+		if (pagament > preu) {
+			canvi = pagament - preu;
+			System.out.println("Sobren " + canvi + "€");
+		} else if (preu > pagament) {
+			canvi = preu - pagament;
+			System.out.println("Falten " + canvi + "€");
+		} else if (preu == pagament) {
+			canvi = preu;
+			System.out.println("No sobra ni falta res");
+		}
+	}
+}
