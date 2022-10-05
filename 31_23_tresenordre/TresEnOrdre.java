062e61cc32c67cce93c94f052c3ee3bef98ee245
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-10-05 20:38:23 +0200

    Finalitzat 31_24  ∾ prgtest resultat 31_24: FALLA

---
+++ 31_23_tresenordre/TresEnOrdre.java
@@ -0,0 +1,19 @@
+/* Programa que demani tres números a l'usuari i els retorni per pantalla ordenats de menor a major. */
+
+public class TresEnOrdre {
+	public static void main(String[] args) {
+		System.out.println("Primer?");
+		int primer = Integer.parseInt(Entrada.readLine();
+
+		System.out.println("Segon?");
+		int segon = Integer.parseInt(Entrada.readLine());
+
+		System.out.println("Tercer?");
+		int tercer = Integer.parseInt(Entrada.readLine());
+
+		if (primer > segon && segon > tercer) {
+			System.out.println(tercer + "," + segon + " i " + primer);
+		} else if (
+
+
+		
