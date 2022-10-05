231ec195cec78796c9ac4d3a30afdf76e3f02c91
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-05 16:40:26 +0200

    Finalitzat 31_22

--- 31_22_mesosanys/MesosAnys.java
+++ 31_22_mesosanys/MesosAnys.java
@@ -4,11 +4,16 @@ public class MesosAnys {
 	public static void main(String [] args) {
 		System.out.println("Mes?");
 		int  month = Integer.parseInt(Entrada.readLine());
-
+			
 		System.out.println("Any?");
-		int year = Integer.parseInt(Entrada.readLine()); 
+		int year = Integer.parseInt(Entrada.readLine());
 
-		System.out.println("Mes anterior " + (month - 1) + "/" + year + " i mes següent " + (month + 1) + "/" + year);
-
+		if (month > 12 || month < 1) {
+			System.out.println("Error");
+		} else if (month == 1) {
+			System.out.println("Mes anterior " + 12 + "/" + (year - 1) + " i mes següent " + (month + 1) + "/" + year);
+		} else if (month > 1 && month < 12) {
+			System.out.println("Mes anterior " + (month - 1) + "/" + year + " i mes següent " + (month + 1) +  "/" + year);
+		}		
 	}
 }
