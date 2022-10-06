76e6f3f76f1adb7a9f627f5155598f85a9134525
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-06 16:35:59 +0200

    Finalitzat 31_27

---
+++ 31_27_nombre_de_fills/NombreDeFills.java
@@ -0,0 +1,22 @@
+/* Programa que pregunta a l'usuari el nombre de fills que tè i segons la quantitat dona una resposta o una altre */
+
+public class NombreDeFills {
+	public static void main(String[] args) {
+		// Preguntem a l'usuari el nombre de fills
+		System.out.println("Quants fills tens?");
+		int numeroFills = Integer.parseInt(Entrada.readLine());
+
+		// Programem la frase que donarà segons el número 
+		if (numeroFills < 0) {
+			System.out.println("No pots tenir menys de 0 fills!");
+		} else if (numeroFills == 0) {
+			System.out.println("Tot el que t'has estalviat en bolquers!");
+		} else if (numeroFills == 1) {
+			System.out.println("Compte de no mimar-lo massa!");
+		} else if (numeroFills > 1 && numeroFills < 5) {
+			System.out.println("No t'avorreixes a casa, eh?");
+		} else if (numeroFills > 4) {
+			System.out.println("Tu sí fas país!");
+		}
+	}
+}
