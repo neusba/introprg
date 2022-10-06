f310e0d05064cd59c98856a139a43db144b26a8c
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-06 14:11:06 +0200

    Finaltizat 31_21

--- 31_21_diessetmana_switch_expr/DiaSetmanaSwitchExpr.java
+++ 31_21_diessetmana_switch_expr/DiaSetmanaSwitchExpr.java
@@ -1,6 +1,6 @@
 /* Programa que demana per entrada estàndard un número entre l'1 i el 7 i torna per pantalla el día de la setmana corresponent, en aquest cas utilizarem la expressió switch i l'operador ternari. */
 
-public class DiaSetmanaSwitchExp {
+public class DiaSetmanaSwitchExpr {
 	public static void main(String[] args) {
 	int number = Integer.parseInt(args[0]);
 		
