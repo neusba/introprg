93068c5fd13a2d944c3bb5ffe6776cc80c4abed5
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-11 18:51:49 +0200

    Finaltizat 31_32

---
+++ 31_32_sumanotes/SumaNotes.java
@@ -0,0 +1,24 @@
+/* Programa que demana les notes dels alumnes i les va sumant sempre i quan estiguin en un rang de 0 a 100, sinó pararà. */
+
+public class SumaNotes {
+	public static void main (String[] args) {
+		
+		// Declarem la suma de les notes i el valor de la nota que donarà l'usuari en unes variables
+		int nota;
+		int sumaNotes = 0;
+
+		// Demanem la primera nota a l'usuari
+		System.out.println("Introdueix una nota");
+		nota = Integer.parseInt(Entrada.readLine());
+		// Fiquem el bucle While
+		while (nota > 0 && nota < 100) {
+			sumaNotes = sumaNotes + nota;
+			// Ens torna a demanar un valor
+			System.out.println("Introdueix una nota");
+			nota = Integer.parseInt(Entrada.readLine());
+		}
+		// Quan el bucli termini, imprimim la suma de les notes
+		System.out.println("La suma de les notes vàlides es " + sumaNotes);
+	}
+}
+		
