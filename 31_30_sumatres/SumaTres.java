e380c0d516e98a47fd1697a92a2ff12c7f5ff331
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-11 17:11:51 +0200

    Finalitzat 31_30

---
+++ 31_30_sumatres/SumaTres.java
@@ -0,0 +1,34 @@
+/* Programa que demana números a l'usuari i els va sumant sempre que siguin positius */
+
+public class SumaTres {
+	public static void main(String[] args) {
+		
+		// Declarem l variable on es guardarà la suma i la que contendrà el valor que l'usuari introdueixi cada vegada
+		int suma = 0;
+		int valor;
+
+		// Demanem el valor a l'usuari i el sumem en cas que sigui positiu
+		System.out.println("Introdueix el primer valor:");
+		valor = Integer.parseInt(Entrada.readLine());
+		if (valor >= 0) {
+			suma = suma + valor;
+		}
+
+		// Demanem el segon valor a l'usuari
+		System.out.println("Introdueix el segon valor:");
+		valor = Integer.parseInt(Entrada.readLine());
+		if (valor >= 0) {
+			suma = suma + valor;
+		}
+
+		// Demanem el tercer valor
+		System.out.println("Introdueix el tercer valor:");
+		valor = Integer.parseInt(Entrada.readLine());
+		if (valor >= 0) {
+			suma = suma + valor;
+		}
+
+		// Imprimim la suma total
+		System.out.println("La suma total es: " + suma);
+	}
+}
