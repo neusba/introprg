d892b241a48f8465cf3afa056749e87e3465e7ea
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-10-12 12:56:28 +0200

    Finalitzat 31_30  ∾ prgtest resultat 31_30: FALLA

---
+++ 31_34_lloro/Lloro.java
@@ -0,0 +1,21 @@
+/* Programa que demani paraules a l'usuari i es vagin repetint fins que es doni una entrada en blanc */
+
+public class Lloro {
+	public static void main(String[] args) {
+
+		// Demanem la paraula a l'usuari
+		System.out.println("El lloro espera la paraula:"); 
+		String word = Entrada.readLine();
+		
+		// Iniciem el bucle while
+		while (word.isEmpty() == false || word.isBlank() == false) {	
+			System.out.println("El lloro repeteix: " + word);
+			// Tornem a demanar la paraula
+			System.out.println("El lloro espera la paraula:");
+			word = Entrada.readLine();
+		}
+		
+		// Finalitzem el programa
+		System.out.println("Adéu");
+	}
+} 
---
+++ exemples/TresEnOrdre.java
@@ -0,0 +1,17 @@
+/* Alternativa al exercici de Tres En Ordre */
+
+public class TresEnOrdre {
+	public static void main(String[] args) {
+		System.out.println("Primer?");
+		int primer = Integer.parseInt(Entrada.readLine());
+
+		System.out.println("Segon?");
+		int segon = Integer.parseInt(Entrada.readLine());
+
+		System.out.println("Tercer?");
+		int tercer = Integer.parseInt(Entrada.readLine());
+
+		int tmp;
+		int aux;
+
+		
