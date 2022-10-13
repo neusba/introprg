86dfb6495f1d1ac24fbe1cc58e188a2bc1a0acaa
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-13 17:14:34 +0200

    Finalitzat 31_39

---
+++ 31_39_endevina/Endevina.java
@@ -0,0 +1,29 @@
+/* Programa que tè per defecte el nombre 42 i demana números a l'usuari fins que encerta, mostra missatges diferents segons si s'allunya o s'apropa o està fora de rang */
+
+public class Endevina {
+	public static void main(String[] args) {
+		// El nombre a encertar és el 42
+			
+		System.out.println("Ves introduïnt enters entre 1 i 100 fins que encertis el que jo he pensat");
+		int valor = Integer.parseInt(Entrada.readLine());
+
+		while (valor >= 1 && valor <= 100) {
+			// Creem les condicions respondre a l'usuari segons la resposta
+			if (valor < 42) {
+				System.out.println("És més gran que " + valor);
+			} else if (valor > 42) {
+				System.out.println("ÉS més petit que " + valor);
+			} else {
+				System.out.println("Has encertat!");
+			}
+		}
+
+		// Si el bucle termina fem les dos condicions restants
+		if (valor <= 0) {
+			System.out.println("Com a mínim 1");
+		} else {
+			System.out.println("Com a màxim 100");
+		}
+	}
+}
+
