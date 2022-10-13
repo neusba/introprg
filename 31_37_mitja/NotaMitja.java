76b633d2f4859c4453a6d06d85112930ac3f3913
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-13 16:25:28 +0200

    Finalitzat 31_37

--- 31_37_mitja/NotaMitja.java
+++ 31_37_mitja/NotaMitja.java
@@ -4,24 +4,24 @@ public class NotaMitja {
 	public static void main(String[] args) {
 
 		// Declarem les variables necessàries
-		int valor = 0; // Inicialitzem en cero per estalviar repetir codi
+		int nota = 0; // Inicialitzem en cero per estalviar repetir codi
 		int acumulador = 0; // Acumulador de notes introduïdes
 		int suma = 0; // Variable per anar sumant totes les notes
 
 		// Creem el bucle while
-		while (valor >= 0 && valor <= 100) {
-			suma = suma + valor;
+		while (nota >= 0 && nota <= 100) {
+			suma = suma + nota;
 			acumulador = acumulador + 1;
 			// Demanem la nota a l'usuari
 			System.out.println("Introdueix un valor");
-			valor = Integer.parseInt(Entrada.readLine());
-		}
+			nota = Integer.parseInt(Entrada.readLine());
+		} 
 		
-		// Creem la condició en cas de que la nota introduïda sigui diferent
-		if ((valor < 0 ||  valor > 100) && acumulador == 1) {
+		if (nota < 0 || nota > 100 && acumulador == 0) {
 			System.out.println("Cap nota vàlida introduïda");
 		} else {
-		       System.out.println("La mitja de les notes vàlides és " + (suma/acumulador));
-	      }	       
+			System.out.println("La mitja de les notes vàlides és " + (suma/acumulador));
+		}
+		 
 	}
 }
