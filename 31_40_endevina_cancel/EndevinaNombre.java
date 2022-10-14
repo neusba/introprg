178d4e871c04c9ec38c53afa01e17c94edcd7ed0
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-14 23:02:45 +0200

    Finalitat 31_40

--- 31_40_endevina_cancel/EndevinaNombre.java
+++ 31_40_endevina_cancel/EndevinaNombre.java
@@ -4,13 +4,39 @@
 public class EndevinaNombre {
 	public static void main(String[] args) {
 		// Declarem les variables
+		System.out.println("Ves introduïnt enters entre 1 i 100 fins que encertis el que jo he pensat");
+		System.out.println("Introdueix un valor");
 		String stringValor = Entrada.readLine();
+		
+		while (stringValor.equals("42") == false || stringValor.isEmpty() == false) {
+			int intValor = Integer.parseInt(stringValor);
+			
+			if (intValor != 42) {
+				if (intValor > 0 && intValor <= 100) {
+					if (intValor < 42) {
+						System.out.println("És més gran que " + intValor);
+					} else {
+						System.out.println("És més petit que " + intValor);
+					} 
+				} else if (intValor > 100) {
+					System.out.println("Com a màxim 100");
+				} else if (intValor <= 0) {
+					System.out.println("Com a mínim 1");
+				}
+			}
+				
+			stringValor = Integer.toString(intValor);
 
-		// Creem el bucle
-		while (stringValor.isNumeric()) {
-			System.out.println("ole");
+			System.out.println("Introdueix un valor");
 			stringValor = Entrada.readLine();
 		}
-		System.out.println("oeee");
+		if (stringValor.isEmpty() == true) {
+			System.out.println("Cancel·lat!");
+		} else if (stringValor.equals("42")) {
+			System.out.println("Has encertat!");
+		}
 	}
 }
+			
+				
+			
