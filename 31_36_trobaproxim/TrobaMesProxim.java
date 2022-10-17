9d1c084f1309dd41a2af4fd613276608f350f510
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-17 18:16:09 +0200

    Finalitzat 31_36

--- 31_36_trobaproxim/TrobaMesProxim.java
+++ 31_36_trobaproxim/TrobaMesProxim.java
@@ -3,37 +3,44 @@
 public class TrobaMesProxim {
 	public static void main(String[] args) {
 
-		// Declarem les variables corresponents
 		int ancora;
-		int valor = 0; // Inicialitzem en cero per estalviar repetir codi
-		int valorMesProxim = 0; // El primer valor més pròxim és igual al primer valor llegit
-		int diferencia; // Diferencia entre el valor i l'àncora
-		int diferenciaValorMesProxim = ancora; // Inicialitzem la diferencia igual a l'ancora perque el primer valor llegit serà 0 i és la diferència entre les dos.
+		int valorMesProxim;
+		int valor = 1;
+		int contador; // Acumulador de valores positivos
+		int diferencia;
+		int diferenciaActual;
+		valorMesProxim = valor; // Comencarà sent el primer valor introduït, 1 en aquest cas
 
-		// Demanem l'ancora a l'usuari
 		System.out.println("Introdueix l'àncora");
 		ancora = Integer.parseInt(Entrada.readLine());
+		diferencia = Math.abs(ancora - valor);
+
 		if (ancora <= 0) {
 			System.out.println("Àncora no vàlida");
-		}
+		} else {
+			while (valor >= 0) {
+				diferenciaActual = Math.abs(ancora - valor);
+				if (diferenciaActual < diferencia) {
+					valorMesProxim = valor;
+				} else if (diferenciaActual == diferencia && valor < valorMesProxim) {
+					valorMesProxim = valor;
+				} 
+				System.out.println("Introdueix un valor");
+				valor = Integer.parseInt(Entrada.readLine());
+			}
 
-		// Creem el bucle while
-		while (valor >= 0) {
-			// Calculem la diferencia que hi ha entre el valor i l'ancora
-			if (valor > ancora) {
-				diferencia = valor - ancora;
-			} else if (valor < ancora) {
-				diferencia = ancora - valor;
-			}
-			// Comparem les diferències del nou valor i la del actual valor més pròxim
-			if (diferencia < diferenciaValorMesProxim) {
-				valorMesProxim = valor; // Si el nou valor tè una diferència més petita que l'anterior, substituim el valor més pròxim actual
-			} else if (diferencia == diferenciaValorMesProxim) {
-				if (valor < valorMesProxim) {
-					valorMesProxim = valor; // Si les diferències són igual, el quedarà el valor més petit
-				}
-			}
+		System.out.println("EL valor introduït més pròxim a " + ancora + " és " + valorMesProxim);
 		}
-		System.out.println("El valor introduït més pròxim a " + ancora + " és " + valorMesProxim);
 	}
 }
+
+
+
+
+
+
+
+
+		
+
+
