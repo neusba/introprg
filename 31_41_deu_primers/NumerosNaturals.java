fc3236be5caec19e7a2790214eb7c63b0b19566c
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-18 20:11:01 +0200

    Finalitzat 31_41

---
+++ 31_41_deu_primers/NumerosNaturals.java
@@ -0,0 +1,15 @@
+/* Programa que escrigui els primers 10 números naturals */
+
+public class NumerosNaturals{
+	public static void main(String[] args) {
+		
+		int contador = 0;
+
+		while (contador < 10) {
+			contador = contador + 1;
+			System.out.println(contador);
+
+		}
+	}
+}
+
