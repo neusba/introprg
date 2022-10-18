91563737c8a9de99ad610293d23dd9f7204d58cd
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-18 19:33:36 +0200

    Finalitzat 31_31

--- 31_31_sumamaxtres/SumaPositius.java
+++ 31_31_sumamaxtres/SumaPositius.java
@@ -1,27 +1,33 @@
-/* Programa que demana valors a l'usuari i els va sumant fins que hi ha un negatiu */
-
 public class SumaPositius {
-	public static void main(String[] args) {
-		
-		// Declarem la variable on anirà la suma i la variable del valor que donarà l'usuari
-		int valor;
-		int suma = 0;
-		int contador = 0;	
-		// Demanem valor a l'usuari
+    public static void main(String[] args) {
+
+        // creem la variable on guardarem el resultat de sumar
+        int suma = 0;   // inicialment no hem sumat res i per tant és 0
+
+        // declarem la variable que contindrà els valors llegits
+        int valor;
+
+        // processem el primer valor
+        System.out.println("Introdueix un valor");
+        valor = Integer.parseInt(Entrada.readLine());
+        if (valor >= 0) {
+            suma = suma + valor;
+
+            // processem el segon valor
+            System.out.println("Introdueix un valor");
+            valor = Integer.parseInt(Entrada.readLine());
+            if (valor >= 0) {
+                suma = suma + valor;
+            	
+		// Procesem el tercer valor
 		System.out.println("Introdueix un valor");
-		valor = Integer.parseInt(Entrada.readLine());
-
-		while (valor >= 0 && contador <= 3) {
-			suma = suma + valor;
-			contador = contador + 1;
-			// Demanem altre valor a l'usuari
-			System.out.println("Introdueix un valor");
-			valor = Integer.parseInt(Entrada.readLine());
+           	valor = Integer.parseInt(Entrada.readLine());
+                if (valor >= 0) {
+                suma = suma + valor;
 		}
-		
-		// Quan el bucle termina, imprimim la suma total dels nombres positius
-		System.out.println("La suma és " + suma);
 	}
+        }	
+        // mostrem el resultat
+        System.out.println("La suma és " + suma);
+    }
 }
-
-
