61710591f606cc627e76ad90b5801a0ea232482e
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-18 22:08:20 +0200

    Finalitzat 31_41

---
+++ 31_41_deu_primers/DeuPrimers.java
@@ -0,0 +1,14 @@
+/* Programa que escrigui els primers 10 números naturals en ordre amb el bucle while */
+
+public class DeuPrimers {
+	public static void main(String[] args) {
+		// Declarem el número
+		int contador = 1;
+
+		// Creem el bucle while
+		while (contador <= 10) {
+			System.out.println(contador);
+			contador += 1;
+		}
+	}
+}
