afce02a3350e91a2acd5ce42f7a7a271e6ad166a
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-19 20:16:31 +0200

    Finalitzat 31_51

--- 31_51_historic_edats/HistoricEdats.java
+++ 31_51_historic_edats/HistoricEdats.java
@@ -5,42 +5,33 @@ public class HistoricEdats {
 		// Declarem variables
 		System.out.println("El vostre nom?");
 		String nom = Entrada.readLine();
-		if (nom.isEmpty()) {
-			System.out.println("Entrada errònia");
-		} else {
-			System.out.println("La vostra edat?");
-			int edat = Integer.parseInt(Entrada.readLine());
-			if (edat < 0) {
-				System.out.println("Entrada errònia");
-			} else {
-				System.out.println("L'any actual?");
-				int anyActual = Integer.parseInt(Entrada.readLine());
-				if (anyActual < 1971) {
-					System.out.println("Entrada errònia");
-				} else {
-					int edatRecorregut = 2;
-			                int anyNaixement = anyActual - edat;
+		System.out.println("La vostra edat?");
+		int edat = Integer.parseInt(Entrada.readLine());
+		System.out.println("L'any actual?");
+		int anyActual = Integer.parseInt(Entrada.readLine());
 
-			                if (edat == 0) {
-                        			System.out.println("Adéu " + nom);
-			                } else if (edat == 1) {
-                        			System.out.println("El " + anyNaixement + " vau néixer");
-			                        System.out.println("Adéu " + nom);
-			                } else if (edat > 1) {
-                       				System.out.println("El " + anyNaixement + " vau néixer");
-                      				System.out.println("El " + (anyNaixement + 1) + " teníeu 1 any");
-			          
-                       				for (int i = anyNaixement + 2; i < anyActual; i++, edatRecorregut++) {
-				              		 System.out.println("El " + i + " teníeu " + edatRecorregut + " anys");
-						}
-						System.out.println("Adéu " + nom);
-					}
-				}
-			}
+		int edatRecorregut = 2;
+                int anyNaixement = anyActual - edat;
+
+		if (nom.isEmpty() || edat < 0 || anyActual < 1971) {
+			System.out.println("Entrada errònia");
+		} else if (edat == 0) {
+			System.out.println("Adéu " + nom);
+                } else if (edat == 1) {
+                        System.out.println("El " + anyNaixement + " vau néixer");
+                        System.out.println("Adéu " + nom);
+                } else if (edat > 1) {
+                        System.out.println("El " + anyNaixement + " vau néixer");
+                        System.out.println("El " + (anyNaixement + 1) + " teníeu 1 any");
+
+                        for (int i = anyNaixement + 2; i < anyActual; i++, edatRecorregut++) {
+                        	System.out.println("El " + i + " teníeu " + edatRecorregut + " anys");
+                        }
+                        
+			System.out.println("Adéu " + nom);
+
 		}
 	}
 }
-
-
 
 	
