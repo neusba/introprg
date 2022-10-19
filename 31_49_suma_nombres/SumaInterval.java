1fbac4586fb1828e3deffb3d1f933ee3ad06fe01
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-19 12:38:02 +0200

    Finalitzat 31_49

---
+++ 31_49_suma_nombres/SumaInterval.java
@@ -0,0 +1,27 @@
+/* Programa que demana 2 números enters a l'usuari i suma tots els que hi ha entre un i l'altre, ambdós inclosos */
+
+public class SumaInterval {
+	public static void main(String[] args) {
+		System.out.println("inici?");
+		int i = Integer.parseInt(Entrada.readLine());
+		System.out.println("inici?");
+		int f = Integer.parseInt(Entrada.readLine());
+		int suma = 0;
+
+		// Si la seqüència es creixent, fem un increment positiu
+		if (i <= f) {
+			for (; i <= f; i++) {
+				suma += i;
+			}
+		} else if (i > f) {
+			for (; i >= f; i--) {
+				suma += i;
+			}
+		}
+
+		// Finalment mostrem la suma
+		System.out.println(suma);
+	}
+}
+
+
