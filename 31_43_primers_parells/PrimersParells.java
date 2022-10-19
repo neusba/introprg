67ed211a96f9a54dc1a277355445bf73902ceec4
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-19 10:50:35 +0200

    Finalitzat 31_43

--- 31_43_primers_parells/PrimersParells.java
+++ 31_43_primers_parells/PrimersParells.java
@@ -4,7 +4,7 @@ public class PrimersParells {
 	public static void main(String[] args) {
 		// Declarem la variable
 		int contador = 0;
-		// Creem el bucle while
+		// Creem el bucle while i finalitzem quan mostri tots el parells fins el 10
 		while (contador < 10) {
 			contador += 2;
 			System.out.println(contador);
