c659ad65fae6817e8fe49fe45153476b077836c6
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-10-27 16:58:17 +0200

    Finalitzat 31_61  ∾ prgtest autocommit 31_61 #1

--- 31_61_informestring/InformeString.java
+++ 31_61_informestring/InformeString.java
@@ -3,48 +3,48 @@
 public class InformeString {
 	public static void main(String[] args) {
 		System.out.println("Text principal?");
-		String stringPrincipal = Entrada.readLine();
+		String stringPrincipal = "String!";
 		System.out.println("Text secundari?");
-		String stringSecundari = Entrada.readLine();
+		String stringSecundari = "string!";
 		System.out.println("Número positiu?");
 		int numero = Integer.parseInt(Entrada.readLine());
 
 		// Informe
-		System.out.print("\"stringPrincipal\".length(): ");
+		System.out.print("\"String!\".length(): ");
 		System.out.println(stringPrincipal.length());
 
-		System.out.print("\"stringPrincipal\".startswith(\"stringSecundari\"): ");
+		System.out.print("\"String!\".startswith(\"string!\"): ");
 		System.out.println(stringPrincipal.startsWith(stringSecundari));
 
-		System.out.print("\"stringPrincipal\".endsWith(\"stringSecundari\"): ");
+		System.out.print("\"String!\".endsWith(\"string!\"): ");
 		System.out.println(stringPrincipal.endsWith(stringSecundari));
 
-		System.out.print("\"stringPrincipal\".equals(\"stringSecundari\"): ");
+		System.out.print("\"String!\".equals(\"string!\"): ");
 		System.out.println(stringPrincipal.equals(stringSecundari));
 
-		System.out.print("\"stringPrincipal\".equalsIgnoreCase(\"stringSecundari\"): ");
+		System.out.print("\"String!\".equalsIgnoreCase(\"string!\"): ");
 		System.out.println(stringPrincipal.equalsIgnoreCase(stringSecundari));
 	
-		System.out.print("\"stringPrincipal\".isBlank(): ");
+		System.out.print("\"String!\".isBlank(): ");
 		System.out.println(stringPrincipal.isBlank());
 
-		System.out.print("\"stringPrincipal\".isEmpty(): ");
+		System.out.print("\"String!\".isEmpty(): ");
 		System.out.println(stringPrincipal.isEmpty());
 		
-		System.out.print("\"stringPrincipal\".charAt()): ");
+		System.out.print("\"String!\".charAt()): ");
 		char ch = stringPrincipal.charAt(numero);
 		System.out.println(ch);
 		
-		System.out.print("\"stringPrincipal\".concat(\"stringSecundari\"): ");
+		System.out.print("\"String!\".concat(\"string!\"): ");
 		System.out.println(stringPrincipal.concat(stringSecundari));
 		
-		System.out.print("\"stringPrincipal\".repeat(): ");
+		System.out.print("\"String!\".repeat(): ");
 		System.out.println(stringPrincipal.repeat(numero));
 
-		System.out.print("\"stringPrincipal\".toUpperCase(): ");
+		System.out.print("\"String!\".toUpperCase(): ");
 		System.out.println(stringPrincipal.toUpperCase());
 
-		System.out.print("\"stringPrincipal\".toLowerCase(): ");
+		System.out.print("\"String!\".toLowerCase(): ");
 		System.out.println(stringPrincipal.toLowerCase());
 	}
 }
