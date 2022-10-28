d391e0175de6a88e107c3b2279dacf9665fec745
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-28 13:18:15 +0200

    Finalitzat 31_61

--- 31_61_informestring/InformeString.java
+++ 31_61_informestring/InformeString.java
@@ -1,52 +1,5 @@
-/* Programa que demana dos String a l'usuari i un número enter positiu */
+/* Programa que demana dos string i un número enter positiu per pantalla i mostra les diferents utilitats de cada métode */
 
-public class InformeString {
+public class InformeString
 	public static void main(String[] args) {
-		System.out.println("Text principal?");
-		String stringPrincipal = Entrada.readLine();
-		System.out.println("Text secundari?");
-		String stringSecundari = Entrada.readLine();
-		System.out.println("Número positiu?");
-		int numero = Integer.parseInt(Entrada.readLine());
-
-		// Informe
-		System.out.print("\"String!\".length(): ");
-		System.out.println(stringPrincipal.length());
-
-		System.out.print("\"String!\".startsWith(\"string!\"): ");
-		System.out.println(stringPrincipal.startsWith(stringSecundari));
-
-		System.out.print("\"String!\".endsWith(\"string!\"): ");
-		System.out.println(stringPrincipal.endsWith(stringSecundari));
-
-		System.out.print("\"String!\".equals(\"string!\"): ");
-		System.out.println(stringPrincipal.equals(stringSecundari));
-
-		System.out.print("\"String!\".equalsIgnoreCase(\"string!\"): ");
-		System.out.println(stringPrincipal.equalsIgnoreCase(stringSecundari));
-	
-		System.out.print("\"String!\".isBlank(): ");
-		System.out.println(stringPrincipal.isBlank());
-
-		System.out.print("\"String!\".isEmpty(): ");
-		System.out.println(stringPrincipal.isEmpty());
-		
-		System.out.print("\"String!\".charAt(3): ");
-		char ch = stringPrincipal.charAt(numero);
-		System.out.println(ch);
-		
-		System.out.print("\"String!\".concat(\"string!\"): ");
-		System.out.println(stringPrincipal.concat(stringSecundari));
-		
-		System.out.print("\"String!\".repeat(3): ");
-		System.out.println(stringPrincipal.repeat(numero));
-
-		System.out.print("\"String!\".toUpperCase(): ");
-		System.out.println(stringPrincipal.toUpperCase());
-
-		System.out.print("\"String!\".toLowerCase(): ");
-		System.out.println(stringPrincipal.toLowerCase());
-	}
-}
-		
 
