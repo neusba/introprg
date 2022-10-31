ab03343a8fb2fbc276c91b8aa1a8289cdbd76b4e
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-31 19:50:14 +0100

    Finalitzat 31_66

---
+++ 31_66_acaba_lletra/LloroAcabaLletra.java
@@ -0,0 +1,21 @@
+/* Nova versió del lloro que repeteix les paraules donades per l'usuari només quan aquestes finalitzen amb una lletra no vocal, s'aturarà quan s'introdueixi uan cadena en blanc */
+
+public class LloroAcabaLletra {
+	public static void main(String[] args) {
+		System.out.println("El lloro pregunta paraula que finalitzi per lletra no vocal");
+		String paraula = Entrada.readLine();
+
+		while (!paraula.isEmpty() && !paraula.isBlank()) {
+			char lletra = paraula.charAt(paraula.length() - 1);
+			String stringLletra = Character.toString(lletra);
+
+			if (!stringLletra.equalsIgnoreCase("a")) {
+				System.out.println("El lloro diu: " + paraula);
+			}
+		System.out.println("El lloro pregunta paraula que finalitzi per lletra no vocal");
+		paraula = Entrada.readLine();
+		}
+		System.out.println("Adéu");
+	}
+}
+			
