332f4a41fc326f3ad6338c4cfbd8050729a882a4
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-31 20:20:05 +0100

    Finalitzat 31_66

--- 31_66_acaba_lletra/LloroAcabaLletra.java
+++ 31_66_acaba_lletra/LloroAcabaLletra.java
@@ -9,9 +9,9 @@ public class LloroAcabaLletra {
 			char lletra = paraula.charAt(paraula.length() - 1);
 			String stringLletra = Character.toString(lletra);
 
-			if (!stringLletra.equalsIgnoreCase("a")) {
-				System.out.println("El lloro diu: " + paraula);
-			}
+				if (!stringLletra.equalsIgnoreCase("a") && !stringLletra.equalsIgnoreCase("e") && !stringLletra.equalsIgnoreCase("i") && !stringLletra.equalsIgnoreCase("o") && !stringLletra.equalsIgnoreCase("u")) { 
+					System.out.println("El lloro diu: " + paraula);
+				}
 		System.out.println("El lloro pregunta paraula que finalitzi per lletra no vocal");
 		paraula = Entrada.readLine();
 		}
