9283da91601d5f2b2e4f2152d5d30dddf83c8255
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-31 19:10:38 +0100

    Finalitzat 31_65

---
+++ 31_65_acaba_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,18 @@
+/* Nova versió del lloro que repeteix les paraules donades per l'usuari només quan aquestes finalitzen amb una vocal en minúscules i s'atura quan rep cadena en blanc. */
+
+public class LloroVocalMinuscules {
+	public static void main(String[] args) {
+		
+		System.out.println("El lloro pregunta paraula que finalitzi per vocal en minúscules");
+		String paraula = Entrada.readLine();
+
+		while (!paraula.isEmpty() && !paraula.isBlank()) {
+			if (paraula.endsWith("a") || paraula.endsWith("e") || paraula.endsWith("i") || paraula.endsWith("o") || paraula.endsWith("u")) {
+				System.out.println("El lloro diu: " + paraula); 
+			}
+			System.out.println("El lloro pregunta paraula que finalitzi per vocal en minúscules");
+			paraula = Entrada.readLine();
+		}
+	}
+}
+			
