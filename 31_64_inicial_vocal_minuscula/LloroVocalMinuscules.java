a85043b5cb2e742f50cc3f82ca521a84922d40a4
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-10-31 18:42:55 +0100

    Finalitzat 31_64

---
+++ 31_64_inicial_vocal_minuscula/LloroVocalMinuscules.java
@@ -0,0 +1,19 @@
+/* Nova versió del programa del lloro, en aquesta ocasió només repeteix les paraules que comencen per una vocal en mínúscules i s'atura quan rep una cadena en blanc */
+
+public class LloroVocalMinuscules {
+	public static void main(String[] args) {
+		System.out.println("EL lloro pregunta paraula que comenci amb vocal en minúscules");
+		String paraula = Entrada.readLine();
+   		
+		while (!paraula.isEmpty()) {
+			if (paraula.startsWith("a") || paraula.startsWith("e") || paraula.startsWith("i") || paraula.startsWith("o") || paraula.startsWith("u")) {
+				System.out.println("El lloro diu: " + paraula);
+			} 
+			System.out.println("EL lloro pregunta paraula que comenci amb vocal en minúscules");
+			paraula = Entrada.readLine();
+		}
+		System.out.println("Adéu");
+	}
+}
+	
+
