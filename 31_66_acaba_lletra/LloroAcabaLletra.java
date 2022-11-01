84b20ca05602c8d62d2c445470299d8f9f3926d7
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-01 19:45:45 +0100

    Finalitzat 31_66  ∾ prgtest autocommit 31_66 #3

--- 31_66_acaba_lletra/LloroAcabaLletra.java
+++ 31_66_acaba_lletra/LloroAcabaLletra.java
@@ -8,10 +8,18 @@ public class LloroAcabaLletra {
 		while (!paraula.isEmpty() && !paraula.isBlank()) {
 			char lletra = paraula.charAt(paraula.length() - 1);
 			String stringLletra = Character.toString(lletra);
-
+			
+			if (Character.isLetter(lletra)) {
 				if (!stringLletra.equalsIgnoreCase("a") && !stringLletra.equalsIgnoreCase("e") && !stringLletra.equalsIgnoreCase("i") && !stringLletra.equalsIgnoreCase("o") && !stringLletra.equalsIgnoreCase("u")) { 
+						System.out.println("El lloro diu: " + paraula);
+				}
+			} else {
+				char penultima = paraula.charAt(paraula.length() - 2);
+				String stringPenultima = Character.toString(penultima);
+				if (!stringPenultima.equalsIgnoreCase("a") && !stringPenultima.equalsIgnoreCase("e") && !stringPenultima.equalsIgnoreCase("i") && !stringPenultima.equalsIgnoreCase("o") && !stringPenultima.equalsIgnoreCase("u")) {// Terminar vocales y listo
 					System.out.println("El lloro diu: " + paraula);
 				}
+			}
 		System.out.println("El lloro pregunta paraula que finalitzi per lletra no vocal");
 		paraula = Entrada.readLine();
 		}
