f7bf5603513c98485619e564cd021fa7f411e8f4
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-02 20:20:46 +0100

    Finalitzat 31_68

---
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -0,0 +1,35 @@
+/* Programa que demana una matrícula a un usuari i li diu si es una matrícula italiana vigent */
+
+public class MatriculaValida {
+	public static void main(String[] args) {
+
+		System.out.println("Introduïu una matrícula");
+		String matricula = Entrada.readLine();
+		
+		char zero = matricula.charAt(0);
+		char primer = matricula.charAt(1);
+		char segon = matricula.charAt(2);
+		char tercer = matricula.charAt(3);
+		char quart = matricula.charAt(4);
+		char cinque = matricula.charAt(5);
+		char sise = matricula.charAt(6);
+
+		if (!Character.isLetter(zero) || !Character.isLetter(primer)) {
+			System.out.println("No és una matrícula italiana vàlida");
+		} else {
+			if (!Character.isDigit(segon) || !Character.isDigit(tercer) || !Character.isDigit(quart)) {
+				System.out.println("No és una matrícula italiana vàlida");
+			} else {
+				if (!Character.isLetter(cinque) || !Character.isLetter(sise)) {
+					System.out.println("No és una matrícula italiana vàlida");
+				} else {
+					System.out.println("És una matrícula italiana vàlida");
+				}
+			}
+		}
+	}
+}
+
+			       	
+
+
