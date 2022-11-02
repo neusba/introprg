08d1eaaaa716da9ded7c36c90fe0b84197184ca3
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-02 20:27:55 +0100

    Finalitzat 31_68  ∾ prgtest autocommit 31_68 #2

--- 31_68_matricula_italiana/MatriculaValida.java
+++ 31_68_matricula_italiana/MatriculaValida.java
@@ -6,17 +6,18 @@ public class MatriculaValida {
 		System.out.println("Introduïu una matrícula");
 		String matricula = Entrada.readLine();
 		
-		char zero = matricula.charAt(0);
-		char primer = matricula.charAt(1);
-		char segon = matricula.charAt(2);
-		char tercer = matricula.charAt(3);
-		char quart = matricula.charAt(4);
-		char cinque = matricula.charAt(5);
-		char sise = matricula.charAt(6);
 		
 		if (matricula.length() < 7) {
 			System.out.println("No és una matrícula italiana vàlida");
 		} else {
+			char zero = matricula.charAt(0);
+			char primer = matricula.charAt(1);
+			char segon = matricula.charAt(2);
+			char tercer = matricula.charAt(3);
+			char quart = matricula.charAt(4);
+			char cinque = matricula.charAt(5);
+			char sise = matricula.charAt(6);
+
 			if (!Character.isLetter(zero) || !Character.isLetter(primer)) {
 				System.out.println("No és una matrícula italiana vàlida");
 			} else {
