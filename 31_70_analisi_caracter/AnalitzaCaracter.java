56a2c4ca347a5cd50471baba1c3f0c42188872bd
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-03 16:53:17 +0100

    Finalitzat 31_70  ∾ prgtest autocommit 31_70 #1

--- 31_70_analisi_caracter/AnalitzaCaracter.java
+++ 31_70_analisi_caracter/AnalitzaCaracter.java
@@ -8,28 +8,24 @@ public class AnalitzaCaracter {
 		System.out.println("Posició?");
 		int posicio = Integer.parseInt(Entrada.readLine());
 		
-		if (Math.abs(posicio) > text.length()) {
+		if (posicio > text.length() || Math.abs(posicio) > text.length() - 1) {
 			System.out.println("Fora de rang");
 		} else {
 			if (posicio < 0) {
 				posicio = text.length() + posicio;
-			}
-			if (Math.abs(posicio) > text.length()) {
-				System.out.println("Fora de rang");
-			} else {
+			} 
 
-				char posicioText = text.charAt(posicio);
+			char posicioText = text.charAt(posicio);
 
-				if (Character.isLetter(posicioText)) {
-					String isLetter = String.format("\'%c\' és una lletra", posicioText);
-					System.out.println(isLetter);
-				} else if (Character.isDigit(posicioText)) {
-					String isDigit = String.format("\'%c\' és un nombre", posicioText);
-					System.out.println(isDigit);
-				} else if (!Character.isLetter(posicioText) && !Character.isDigit(posicioText)) {
-					String altraCosa = String.format("\'%c\' és una altra cosa", posicioText);
-					System.out.println(altraCosa);
-				}
+			if (Character.isLetter(posicioText)) {
+				String isLetter = String.format("\'%c\' és una lletra", posicioText);
+				System.out.println(isLetter);
+			} else if (Character.isDigit(posicioText)) {
+				String isDigit = String.format("\'%c\' és un nombre", posicioText);
+				System.out.println(isDigit);
+			} else if (!Character.isLetter(posicioText) && !Character.isDigit(posicioText)) {
+				String altraCosa = String.format("\'%c\' és una altra cosa", posicioText);
+				System.out.println(altraCosa);
 			}
 		}
 	}
