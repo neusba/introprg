14d6d81f53fda19a4f93e024992cb9b7f8380ff6
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-03 22:15:07 +0100

    Finalitzat 31_72

---
+++ 31_72_nomeslletres/NomesLletres.java
@@ -0,0 +1,20 @@
+/* Programa que demana un text i mostra només les lletres que conté. Cada lletra apareixerà separada per una coma, els altrs signes no es mostraran per pantalla */
+
+public class NomesLletres {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+
+		for (int posicio=0; posicio <= text.length() - 1; posicio++) {
+			if (Character.isLetter(text.charAt(posicio))) { 
+				if (posicio == 0) {
+					System.out.print(text.charAt(posicio) + ",");
+				} else if (posicio == text.length()) {
+					System.out.print(" " + text.charAt(posicio));
+				} else {
+					System.out.print(" " + text.charAt(posicio) + ",");
+				}
+			}
+		}
+	}
+}
