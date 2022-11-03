b546355f064ce1330e28175244c77b563e5ebe08
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-03 23:43:55 +0100

    Finalitzat 31_72  ∾ prgtest autocommit 31_72 #10

--- 31_72_nomeslletres/NomesLletres.java
+++ 31_72_nomeslletres/NomesLletres.java
@@ -4,17 +4,20 @@ public class NomesLletres {
 	public static void main(String[] args) {
 		System.out.println("Text?");
 		String text = Entrada.readLine();
+		String textLletres = "";
 
 		for (int posicio=0; posicio < text.length(); posicio++) {
-			if (Character.isLetter(text.charAt(posicio))) { 
-				System.out.print(text.charAt(posicio) + ",");
-				
-				if (posicio == text.length() - 2) {
-					System.out.print(" " + text.charAt(posicio));
-				} else {
-				       	System.out.print(" " + text.charAt(posicio) + ",");
-				}
-		 			
+			if (Character.isLetter(text.charAt(posicio))) {
+				textLletres = textLletres + text.charAt(posicio);
+			}
+		}
+		for (int lletres=0; lletres < textLletres.length(); lletres++) {
+			if (lletres == 0) {
+				System.out.print(textLletres.charAt(lletres) + ",");
+			} else if (lletres == textLletres.length() - 1) {
+				System.out.print(" " + textLletres.charAt(lletres));
+			} else {
+				System.out.print(" " + textLletres.charAt(lletres) + ",");
 			}
 		}
 	}
