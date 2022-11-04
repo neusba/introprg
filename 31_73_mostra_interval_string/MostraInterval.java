402192ddc51090cc0d8d9be1d1a9d6c89d492e95
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-04 22:42:35 +0100

    Finalitzat 31_73  ∾ prgtest autocommit 31_73 #8

--- 31_73_mostra_interval_string/MostraInterval.java
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -10,18 +10,18 @@ public class MostraInterval {
 		int valorFinal = Integer.parseInt(Entrada.readLine());
 				
 		if (inici < 0 || valorFinal > text.length() - 1) {
-			if (inici < 0) {
-				for (int a=0; a <= valorFinal; a++) {
+			if (inici < 0 && valorFinal > text.length() - 1) {
+				for (int a=0; a <= text.length() - 1; a++) {
 					System.out.println(text.charAt(a));
 				}
 			} else if (valorFinal > text.length() - 1) {
 				for (; inici < text.length() - 1; inici ++) {
 					System.out.println(text.charAt(inici));
 				}	
-			} else if (inici < 0 && valorFinal > text.length() - 1) {
-				for (int a=0; a <= text.length() - 1; a++) {
+			} else if (inici < 0) {
+				for (int a=0; a <= valorFinal; a++) {
 					System.out.println(text.charAt(a));
-			}
+				}
 			}	
 		} else if (inici <= valorFinal) {
 			for (; inici <= valorFinal; inici++) {
