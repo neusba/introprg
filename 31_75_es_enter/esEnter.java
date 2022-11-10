6c10a687da559e2d41a236496742461e7c1e1687
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-10 17:09:37 +0100

    Finalitzat 31_75

---
+++ 31_75_es_enter/esEnter.java
@@ -0,0 +1,26 @@
+/* Programa que demana texts per entrada fins que rep un buit, quan no sigui buit ens dirà si és un nombre enter escrit amb dígits tot ignorant els espais extra a l'inici i al final del text i es pot acceptar el signe +/- */
+
+public class esEnter {
+	public static void main(String[] args) {
+		System.out.println("Introdueix texts (enter sol per finalitzat)");
+		String text = Entrada.readLine();
+
+		while (!text.isEmpty()) {
+			String stripText = text.strip();
+			
+			if (Character.isLetter(stripText.charAt(0))) {
+				System.out.println("No és enter");
+			} else {
+				int intStripText = Integer.parseInt(stripText);
+				if (intStripText >= 0 || intStripText < 0) {
+					System.out.println("És enter");
+				}
+			}
+			text = Entrada.readLine();
+		}
+		System.out.println("Adéu");
+	}
+}
+					
+				
+				
