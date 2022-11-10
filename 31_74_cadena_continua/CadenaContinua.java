2691bf848eeab97abf2417fb8fdde87ffbd8b572
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-10 16:10:08 +0100

    Finalitzat 31_74

--- 31_74_cadena_continua/CadenaContinua.java
+++ 31_74_cadena_continua/CadenaContinua.java
@@ -12,11 +12,11 @@ public class CadenaContinua {
 			int nombre = Integer.parseInt(Entrada.readLine());
 			if (nombre >= 1) {
 				int modul = nombre % (text.length()); // Serán les lletres extres després de la paraula estàndar. 
-				int repeticio = nombre / text.length(); // La repetició és les vegades que es repetirà la paraula sencera per omplicar el recorregut dels caràcter demanats
+				int repeticio = nombre / text.length(); // La repetició és les vegades que es repetirà la paraula sencera per omplir el recorregut dels caràcter demanats
 				if (modul == 0) {
-					System.out.print(text.repeat(repeticio)); // En aquest cas la repeticio la utilitzem per saber quantes vegades és la paraula sencera en tot el recorregut	
+					System.out.print(text.repeat(repeticio)); // Si el mòdul eś 0 no podem saber les lletres restants, però amb la repetició sabem quantes vegades es repeteix la paraula en el recorregut	
 				} else if (modul != 0) {
-					System.out.print(text.repeat(repeticio)); // En aquest cas la repeticio la utilitzem per saber quantes vegades és la paraula sencera en tot el recorregut
+					System.out.print(text.repeat(repeticio)); // En aquest cas la repeticio la utilitzem per saber quantes vegades està la paraula estàndar abans de les lletres extres
 					for (int inici=0; inici < modul; inici++) {
 						System.out.print(text.charAt(inici));
 					}
