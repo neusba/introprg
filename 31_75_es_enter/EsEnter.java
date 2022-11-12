791713f23b81a4f14dac38349f803ab0e09b0d47
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-12 21:05:36 +0100

    Finalitzat 31_75  ∾ prgtest autocommit 31_75 #40

--- 31_75_es_enter/EsEnter.java
+++ 31_75_es_enter/EsEnter.java
@@ -18,13 +18,13 @@ public class EsEnter {
 					contadorSignes += 1;
 				}
 			}
-			if (contadorLletres >= 1) {
-				System.out.println("No és enter");
-			} else if (contadorLletres == 0) {
-				System.out.println("És enter");
-			} else if (contadorSignes >= 2) {
+			if (contadorSignes >= 2) {
 				System.out.println("No és enter");
 			} else if (contadorSignes <= 1) { 
+				System.out.println("És enter");
+			} else if (contadorLletres >= 1) {
+				System.out.println("No és enter");
+			} else if (contadorLletres == 0) {
 				System.out.println("És enter");
 			}
 		text = Entrada.readLine();
