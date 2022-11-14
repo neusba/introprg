c69d79bc773bc5e1e62254e7938d0441a7ed275c
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-14 16:48:57 +0100

    Finalitat 31_76  ∾ prgtest autocommit 31_76 #15

--- 31_76_cerca_x/ConteDarrer.java
+++ 31_76_cerca_x/ConteDarrer.java
@@ -4,31 +4,34 @@ public class ConteDarrer {
 	public static void main(String[] args) {
 		System.out.println("Introdueix texts (enter sol per finalitzar)");
 		String text = Entrada.readLine();
-		text = text.toLowerCase();
 		int contadorText = 0;
-		char darreraLletra = text.charAt(text.length() - 1);
-
-		while (!text.isEmpty()) {
-			contadorText += 1;
-			boolean conteLletra = false;
-			for (int i=0; i < text.length(); i++) {
-				if (text.charAt(i) == darreraLletra) {
-					conteLletra = true;
+		
+		if (!text.isEmpty()) {	
+			char darreraLletra = text.charAt(text.length() - 1);
+			while (!text.isEmpty()) {
+				text = text.toLowerCase();
+				contadorText += 1;
+				boolean conteLletra = false;
+				for (int i=0; i < text.length(); i++) {
+					if (text.charAt(i) == darreraLletra) {
+						conteLletra = true;
+					}
 				}
+				if (conteLletra) {
+					System.out.println("bé");
+				} else {
+					break;
+				}
+				char darreraNovaLletra = text.charAt(text.length() - 1);	
+				text = Entrada.readLine();
+				text = text.toLowerCase();
+				if (contadorText >= 2) {
+					char auxiliar = darreraLletra;
+					darreraLletra = darreraNovaLletra;
+				}	
 			}
-			if (conteLletra) {
-				System.out.println("bé");
-			} else {
-				break;
-			}
-			char darreraNovaLletra = text.charAt(text.length() - 1);	
-			text = Entrada.readLine();
-			text = text.toLowerCase();
-			if (contadorText >= 2) {
-				char auxiliar = darreraLletra;
-				darreraLletra = darreraNovaLletra;
-			}	
 		}
+		// OUT
 		System.out.println("Adéu");
 	}
 }				
