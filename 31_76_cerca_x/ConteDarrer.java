ee6014d6cfd5937c381d1c778404aaa3d9cbbf3b
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-14 17:10:17 +0100

    Finalitat 31_76  ∾ prgtest autocommit 31_76 #24

--- 31_76_cerca_x/ConteDarrer.java
+++ 31_76_cerca_x/ConteDarrer.java
@@ -4,9 +4,9 @@ public class ConteDarrer {
 	public static void main(String[] args) {
 		System.out.println("Introdueix texts (enter sol per finalitzar)");
 		String text = Entrada.readLine();
+		text = text.toLowerCase();
 		int contadorText = 0;
 		char darreraLletra = text.charAt(text.length() - 1);
-		text = text.toLowerCase();
 
 		while (!text.isEmpty()) {
 			contadorText += 1;
