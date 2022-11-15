72aaa2a072fca6c5147e63aec110ddea11608c75
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-15 13:19:38 +0100

    Canvis a l'exercici 31_73  ∾ prgtest autocommit 31_73 #6

--- 31_73_mostra_interval_string/MostraInterval.java
+++ 31_73_mostra_interval_string/MostraInterval.java
@@ -38,6 +38,10 @@ public class MostraInterval {
 					for (; inici <= valorFinal; inici++) {
 						System.out.println(text.charAt(inici));
 					}
+				} else {
+					for (; inici >= valorFinal; inici--) {
+						System.out.println(text.charAt(inici));
+					}
 				}
 			} else if (valorFinal > text.length() - 1) {
 				for (; inici <= text.length() - 1; inici++) {
