3c45706ddb1ae67fb809e8a49ccca53d3bc141b8
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-16 13:17:35 +0100

    Finalitzat 31_86

---
+++ 31_86_trianglelletres/TriangleLletres.java
@@ -0,0 +1,20 @@
+/* Programa que demana un text i dibuixi un triangle amb les lletres del text */
+
+public class TriangleLletres {
+	public static void main(String[] args) {
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+
+		for (int i=0; i < text.length(); i++) {
+			for (int j=0; j <= i; j++) {
+				if (j == text.length() - 1) {
+					System.out.print(text.charAt(j));
+				} else {
+					System.out.print(text.charAt(j) + ", ");
+				}	
+			}
+			System.out.println();
+		}
+	}
+}
+
