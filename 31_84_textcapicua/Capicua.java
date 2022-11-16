71bb8633d845056a9b033a58aa8c975783556375
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-16 12:13:44 +0100

    Finalitzat 31_84  ∾ prgtest autocommit 31_84 #1

--- 31_84_textcapicua/Capicua.java
+++ 31_84_textcapicua/Capicua.java
@@ -6,13 +6,17 @@ public class Capicua {
 		String text = Entrada.readLine();
 		String textReves = "";
 
-		for (int i=text.length() - 1; i >= 0; i--) {
-			textReves = textReves + text.charAt(i);
-		}
-		if (text.equalsIgnoreCase(textReves)) {
-			System.out.println("És capicua");
-		} else {
+		if (text.isEmpty()) {
 			System.out.println("No és capicua");
+		} else {
+			for (int i=text.length() - 1; i >= 0; i--) {
+				textReves = textReves + text.charAt(i);
+			}
+			if (text.equalsIgnoreCase(textReves)) {
+				System.out.println("És capicua");
+			} else {
+				System.out.println("No és capicua");
+			}
 		}
 	}
 }
