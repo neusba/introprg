b3ad7e0df8cbb8f9e0d99a92dc3d946a0de8fd5e
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-17 01:34:47 +0100

    Finalitzat 31_81

--- 31_81_inicialsmajuscules/MajusculitzaInicials.java
+++ 31_81_inicialsmajuscules/MajusculitzaInicials.java
@@ -5,12 +5,29 @@ public class MajusculitzaInicials {
 		System.out.println("Text?");
 		String text = Entrada.readLine();
 		String nouText = "";
-		boolean whiteSpace = false;
+		boolean primeraLletra = false;
 
 		if (text.isEmpty()) {
 			System.out.println("Cadena buida");
 		} else {
-			
-			
-		
+			text = text.toLowerCase();
+			for (int i=0; i < text.length(); i++) {
+				if (Character.isLetter(text.charAt(i))) {
+					if (!primeraLletra) {
+						nouText = nouText + Character.toUpperCase(text.charAt(i));
+						primeraLletra = true;
+					} else if (Character.isWhitespace(text.charAt(i - 1)) || (!Character.isLetter(text.charAt(i - 1)) && !Character.isDigit(text.charAt(i - 1)))) {
+						nouText = nouText + Character.toUpperCase(text.charAt(i));
+					} else {
+						nouText = nouText + text.charAt(i);
+					}
+				} else {
+					nouText = nouText + text.charAt(i);
+				}
+			}
+			System.out.println(nouText);
+		}
+	}
+}
 
+			
