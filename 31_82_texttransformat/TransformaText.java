96d5fbaca93289b36e4f9a2eb288621058d87a46
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-17 11:44:51 +0100

    Corregint 31_82

--- 31_82_texttransformat/TransformaText.java
+++ 31_82_texttransformat/TransformaText.java
@@ -6,8 +6,39 @@ public class TransformaText {
 		String text = Entrada.readLine();
 		String vocals = "AEIOUÁÉÈÍÏÓÒÚÜ";
 		String nouText = "";
-		char letra = text.charAt(0); // Inicialitzem en 0 per poder fer-ho simplement
+		boolean conteVocal;
 
 		if (text.isEmpty()) {
 			System.out.println("Cadena buida");
 		} else {
+			text = text.toUpperCase();
+			for (int i=0; i < text.length(); i++) {
+				conteVocal = false;
+				if (Character.isLetter(text.charAt(i))) {
+					for (int j=0; j < vocals.length(); j ++) {
+						if (text.charAt(i) == vocals.charAt(j)) {
+							nouText = nouText + Character.toUpperCase(text.charAt(i));
+							conteVocal = true;
+						}
+					}
+					if (!conteVocal) {
+						nouText = nouText + text.charAt(i);
+					}
+				} else if (Character.isDigit(text.charAt(i))) {
+					if (Character.isWhitespace(text.charAt(i - 1)) || text.charAt(i) == 0) {
+						nouText = nouText + "(" + text.charAt(i);
+					} else if (text.charAt(i) == text.length() - 1) {
+						nouText = nouText + text.charAt(i) + ")";
+					}
+				} else if (Character.isWhitespace(text.charAt(i))) {
+					if (Character.isDigit(text.charAt(i - 1))) {
+						nouText = nouText + ") ";
+					} else {
+						nouText = nouText + text.charAt(i);
+					}
+				}
+			}
+			System.out.println(nouText);	
+		}
+	}
+}
