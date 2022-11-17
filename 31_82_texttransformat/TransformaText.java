d761f5876c6845344d6023d06e67f5afa168702d
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-17 12:14:01 +0100

    Corregint 31_82  ∾ prgtest autocommit 31_82 #13

--- 31_82_texttransformat/TransformaText.java
+++ 31_82_texttransformat/TransformaText.java
@@ -8,43 +8,39 @@ public class TransformaText {
 		String nouText = "";
 		boolean conteVocal;
 
-		if (text.isEmpty()) {
-			System.out.println("Cadena buida");
-		} else {
-			text = text.toUpperCase();
-			for (int i=0; i < text.length(); i++) {
-				conteVocal = false;
-				if (Character.isLetter(text.charAt(i))) {
-					for (int j=0; j < vocals.length(); j ++) {
-						if (text.charAt(i) == vocals.charAt(j)) {
-							nouText = nouText + Character.toLowerCase(text.charAt(i));
-							conteVocal = true;
-						}
+		text = text.toUpperCase();
+		for (int i=0; i < text.length(); i++) {
+			conteVocal = false;
+			if (Character.isLetter(text.charAt(i))) {
+				for (int j=0; j < vocals.length(); j ++) {
+					if (text.charAt(i) == vocals.charAt(j)) {
+						nouText = nouText + Character.toLowerCase(text.charAt(i));
+						conteVocal = true;
 					}
-					if (!conteVocal) {
-						nouText = nouText + text.charAt(i);
-					}
-				} else if (Character.isDigit(text.charAt(i))) {
-					if (text.charAt(i) == text.charAt(0)) {
-						nouText = nouText + "(" + text.charAt(i);
-					} else if (Character.isDigit(text.charAt(i - 1))) {
-						if (text.charAt(i) == text.charAt(text.length() -1)) {
-							nouText = nouText + text.charAt(i) + ")";
-						} else {
-							nouText = nouText + text.charAt(i);
-						}
-					} else if (Character.isWhitespace(text.charAt(i - 1))) {
-						nouText = nouText + "(" + text.charAt(i);
-					}
-				} else if (Character.isWhitespace(text.charAt(i))) {
-					if (Character.isDigit(text.charAt(i - 1))) {
-						nouText = nouText + ") ";
+				}
+				if (!conteVocal) {
+					nouText = nouText + text.charAt(i);
+				}
+			} else if (Character.isDigit(text.charAt(i))) {
+				if (text.charAt(i) == text.charAt(0)) {
+					nouText = nouText + "(" + text.charAt(i);
+				} else if (Character.isDigit(text.charAt(i - 1))) {
+					if (text.charAt(i) == text.charAt(text.length() -1)) {
+						nouText = nouText + text.charAt(i) + ")";
 					} else {
 						nouText = nouText + text.charAt(i);
 					}
+				} else if (Character.isWhitespace(text.charAt(i - 1))) {
+					nouText = nouText + "(" + text.charAt(i);
+				}
+			} else if (Character.isWhitespace(text.charAt(i))) {
+				if (Character.isDigit(text.charAt(i - 1))) {
+					nouText = nouText + ") ";
+				} else {
+					nouText = nouText + text.charAt(i);
 				}
 			}
-			System.out.println(nouText);	
+		System.out.println(nouText);	
 		}
 	}
 }
