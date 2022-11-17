61fdfbeaa2d0996a210407b8dd89cc6da93c5c52
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-17 13:07:19 +0100

    Simplificat 31_78

--- 31_78_vocalsmajuscules/VocalsMajuscules.java
+++ 31_78_vocalsmajuscules/VocalsMajuscules.java
@@ -1,29 +1,31 @@
-/* Programa que demani un text i mostri les lletres en minúscules menys les vocals que han d'estar en majúscula */
+/* Programa que demana un text i el retorna amb totes les lletres en minúscules excepte les vocals que hauran d'estar en majúscules */
 
 public class VocalsMajuscules {
-	public static void main(String[] args) {
-		System.out.println("Text?");
-		String text = Entrada.readLine();
-		text = text.toLowerCase();
-		String vocals = "aeiou";
-		char letra = text.charAt(0);
-		String nouText = "";
+        public static void main(String[] args) {
+                System.out.println("Text?");
+                String text = Entrada.readLine();
+                text = text.toLowerCase();
+                String vocals  = "aeiou";
+                boolean conteVocal;
+                String nouText = "";
 
-		for (int i=0; i < text.length(); i++) {
-			if (Character.isLetter(text.charAt(i))) {
-				for (int j=0; j < vocals.length(); j++) {
-					letra = text.charAt(i);
-					if (text.charAt(i) == vocals.charAt(j)) {
-						letra = Character.toUpperCase(text.charAt(i));
-						break;
-					}
-				}
-			} else { 
-				letra = text.charAt(i);
-			}
-			nouText = nouText + letra;
-		}
-		System.out.println(nouText);
-		}
-	}
-			
+                for (int i=0; i < text.length(); i++) {
+                        conteVocal = false;
+                        if (Character.isLetter(text.charAt(i))) {
+                                for (int j=0; j < vocals.length(); j++) {
+                                       if (text.charAt(i) == vocals.charAt(j)) {
+                                               nouText = nouText + Character.toUpperCase(text.charAt(i));
+                                               conteVocal = true;
+                                       }
+                                }
+                                if (!conteVocal) {
+                                        nouText = nouText + text.charAt(i);
+                                }
+                        } else {
+                                nouText = nouText + text.charAt(i);
+                        }
+                }
+                System.out.println(nouText);
+        }
+}
+	
