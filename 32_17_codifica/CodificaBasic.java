df9bde95b10a2d81c7f7ffdc1289787eb8996396
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-28 16:27:59 +0100

    Finalitzat 32_17

---
+++ 32_17_codifica/CodificaBasic.java
@@ -0,0 +1,28 @@
+/* Versió millorada del exercici 32_85, el qual consistia en codificar un text donat per l'usuari, aquesta vegada es codificarà segons el nombre que l'usuari decideixi */
+
+public class CodificaBasic {
+	public static void main(String[] args) {
+		// obtenim les dades
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		System.out.println("Quants?");
+		int quants = Integer.parseInt(Entrada.readLine());
+		if (quants < 0) {
+			System.out.println("No s'accepten números negatius");
+			return;
+		}
+		codifica(text, quants);
+	}
+	// codifiquem el text
+	public static void codifica(String text, int quants) {
+		for (int i=0; i < text.length(); i++) {
+			if (text.charAt(i) >= 'a' && text.charAt(i) <= 'z') {
+				if (text.charAt(i) == 'z') {
+					System.out.print((char)('a' + (quants - 1)));
+				} else {
+					System.out.print((char)(text.charAt(i) + quants));
+				}
+			}
+		}
+	}	
+}
