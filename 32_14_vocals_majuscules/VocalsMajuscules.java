b3ec64e2cf25418e1b228d8b08f7ae753f0ee984
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-28 15:42:44 +0100

    Finalitzat 32_14

---
+++ 32_14_vocals_majuscules/VocalsMajuscules.java
@@ -0,0 +1,32 @@
+/* Versió millorada del exercici 31_78, el qual consistia en transformar les vocals d'un text donar per l'usuari en majúscules  */
+
+public class VocalsMajuscules {
+	public static void main(String[] args) {
+		// obtenim les dades
+		System.out.println("Text?");
+		String text = Entrada.readLine();
+		majusculitzaVocals(text);
+	}
+	// transformem el text
+	public static void majusculitzaVocals(String text) {
+		String vocals = "aeiou";
+		String nouText = "";
+		for (int i=0; i < text.length(); i++) {
+			boolean vocal = false;
+			if (Character.isLetter(text.charAt(i))) {
+				for (int j=0; j < vocals.length(); j++) {
+					if (text.charAt(i) == vocals.charAt(j)) {
+						nouText = nouText + Character.toUpperCase(text.charAt(i));
+						vocal = true;
+					}
+				}
+				if (!vocal) {
+					nouText = nouText + text.charAt(i);
+				}
+			} else {
+				nouText = nouText + Character.toUpperCase(text.charAt(i));
+			}
+		}
+		System.out.println(nouText);
+	}
+}
