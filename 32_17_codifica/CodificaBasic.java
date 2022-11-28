671583962707e6a165dbd31fc3b01cce0e1f7cc2
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-28 16:31:56 +0100

    Finalitzat 32_17  ∾ prgtest autocommit 32_17 #2

--- 32_17_codifica/CodificaBasic.java
+++ 32_17_codifica/CodificaBasic.java
@@ -17,10 +17,12 @@ public class CodificaBasic {
 	public static void codifica(String text, int quants) {
 		for (int i=0; i < text.length(); i++) {
 			if (text.charAt(i) >= 'a' && text.charAt(i) <= 'z') {
-				if (text.charAt(i) == 'z') {
-					System.out.print((char)('a' + (quants - 1)));
-				} else {
-					System.out.print((char)(text.charAt(i) + quants));
+				if (quants > 1) {
+					if (text.charAt(i) == 'z') {
+						System.out.print((char)('a' + (quants - 1)));
+					} else {
+						System.out.print((char)(text.charAt(i) + quants));
+					}
 				}
 			} else {
 				System.out.print(text.charAt(i));
