10eb0e32fdb2f25b978d6d17f6bc1f05a089d918
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-11-30 21:53:31 +0100

    Finalitzat 32_18  ∾ prgtest autocommit 32_18 #5

--- 32_18_compara_ocurrencies/ComptaAiEs.java
+++ 32_18_compara_ocurrencies/ComptaAiEs.java
@@ -5,23 +5,20 @@ public class ComptaAiEs {
 		// obtenim les dades
 		System.out.println("Introdueix un text");
 		String text = Entrada.readLine();
-		comptaLletra(text, 'a', 'e');
+		comptaLletra(text, 'a');
+		comptaLletra(text, 'e');
 		comparaOcurrencies(text, 'a', 'e');
 	
 	}
 	// comptem els dos caracters
-	public static void comptaLletra(String text, char lletra1, char lletra2) {
-		int comptadorA = 0;
-		int comptadorB = 0;
+	public static void comptaLletra(String text, char lletra) {
+		int comptador = 0;
 		for (int i=0; i < text.length(); i++) {
 			if (text.charAt(i) == 'a') {
-				comptadorA += 1;
-			} else if (text.charAt(i) == 'e') {
-				comptadorB += 1;
+				comptador += 1;
 			}
 		}
-		System.out.println("Nombre de 'a's: " + comptadorA);
-		System.out.println("Nombre de 'e's: " + comptadorB);
+		System.out.println("Nombre de \'" + lletra + "\'s: " + comptador);
 	}
 	// comparem el caràcters
 	public static void comparaOcurrencies(String text, char lletra1, char lletra2) {
