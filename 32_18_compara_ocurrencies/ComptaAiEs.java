3fef0dadd9f0ece7775efad926beb2d7fb17c738
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-11-30 21:47:24 +0100

    Finalitzat 32_18

---
+++ 32_18_compara_ocurrencies/ComptaAiEs.java
@@ -0,0 +1,45 @@
+/* Programa que compta i compara les ocurrències de dos caràcters d'un string donat per l'usuari */
+
+public class ComptaAiEs {
+	public static void main(String[] args) {
+		// obtenim les dades
+		System.out.println("Introdueix un text");
+		String text = Entrada.readLine();
+		comptaLletres(text, 'a', 'e');
+		comparaOcurrencies(text, 'a', 'e');
+	
+	}
+	// comptem els dos caracters
+	public static void comptaLletres(String text, char lletra1, char lletra2) {
+		int comptadorA = 0;
+		int comptadorB = 0;
+		for (int i=0; i < text.length(); i++) {
+			if (text.charAt(i) == 'a') {
+				comptadorA += 1;
+			} else if (text.charAt(i) == 'b') {
+				comptadorB += 1;
+			}
+		}
+		System.out.println("Nombre de 'a's; " + comptadorA);
+		System.out.println("Nombre de 'b's; " + comptadorB);
+	}
+	// comparem el caràcters
+	public static void comparaOcurrencies(String text, char lletra1, char lletra2) {
+ 		int comptadorA = 0;
+        	int comptadorE = 0;
+        	for (int i=0; i < text.length(); i++) {
+            		if (text.charAt(i) == 'a') {
+				comptadorA += 1;
+            		} else if (text.charAt(i) == 'e') {
+                		comptadorE += 1; 
+			}
+        	}
+        	if (comptadorA > comptadorE) {
+       			System.out.println("Hi ha més 'a's que 'e's");
+        	} else if (comptadorA < comptadorE) {
+            		System.out.println("Hi ha menys 'a's que 'e's");
+        	} else {
+            		System.out.println("Hi ha tantes 'a's com 'e's");
+		}
+	}
+}
