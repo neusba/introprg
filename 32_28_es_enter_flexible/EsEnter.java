c777a0ea30d2cdeca5606341438d592d0af0c385
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-12-12 17:19:10 +0100

    Finalitzat 32_29

---
+++ 32_28_es_enter_flexible/EsEnter.java
@@ -0,0 +1,7 @@
+/* Versió flexible del exercici anterior 'És enter'
+ * Aquesta vegada crearem la possiblitat d'acceptar números amb espais en blanc
+ */
+
+public class EsEnter {
+	public static void main(String[] args) {
+
--- 32_29_cadena_continua/UtilString.java
+++ 32_29_cadena_continua/UtilString.java
@@ -3,21 +3,6 @@
  */
 
 public class UtilString {
-	public static String cadenaContinua(String text, int longitudInt) {
-		String cadenaContinua = "";
-		// fem el mòdul de la longitud demanada entre la longitud del text
-		int modul = longitudInt % (text.length()); // Serán les lletres extres després de la paraula estàndar. 
-		int repeticio = longitudInt / text.length(); // La repetició és les vegades que es repetirà la paraula sencera per omplir el recorregut dels caràcter demanats
-		if (modul == 0) {
-			cadenaContinua = text.repeat(repeticio);  // però amb la repetició sabem quantes vegades es repeteix la paraula en el recorregut	
-		} else if (modul != 0) {
-			cadenaContinua = text.repeat(repeticio); // En aquest cas la repeticio la utilitzem per saber quantes vegades està la paraula estàndar abans de les lletres extres
-			for (int inici=0; inici < modul; inici++) {
-				cadenaContinua = text.repeat(repeticio);
-			}
-		}
-		return cadenaContinua;
-	}
 	// Funció per calcular si un string és un nombre enter o no
 	public static boolean esEnter(String longitud) {
 		if (longitud.isEmpty()) {
@@ -33,5 +18,23 @@ public class UtilString {
 			}
 		}
 		return true;
+	}
+	// Funció que retorna la cadena continua amb el caràcters indicats
+	public static String cadenaContinua(String text, int longitudInt) {
+		String cadenaContinua = "";
+		// fem el mòdul de la longitud demanada entre la longitud del text
+		int modul = longitudInt % (text.length()); // Serán les lletres extres després de la paraula estàndar. 
+		int repeticio = longitudInt / text.length(); // La repetició és les vegades que es repetirà la paraula sencera per omplir el recorregut dels caràcter demanats
+		if (modul == 0) {
+			text = text.repeat(repeticio);
+			cadenaContinua = cadenaContinua + text;  // però amb la repetició sabem quantes vegades es repeteix la paraula en el recorregut	
+		} else if (modul != 0) {
+			text = text.repeat(repeticio);
+			cadenaContinua = cadenaContinua + text; // En aquest cas la repeticio la utilitzem per saber quantes vegades està la paraula estàndar abans de les lletres extres
+			for (int inici=0; inici < modul; inici++) {
+				cadenaContinua = cadenaContinua + text.repeat(repeticio);
+			}
+		}
+		return cadenaContinua;
 	}
 }
