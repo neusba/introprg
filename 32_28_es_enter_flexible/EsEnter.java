c601e9fb865df6e4110fd612200308c98ddfd52b
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-12-13 17:25:53 +0100

    finalitzat 32_28

--- 32_28_es_enter_flexible/EsEnter.java
+++ 32_28_es_enter_flexible/EsEnter.java
@@ -4,4 +4,24 @@
 
 public class EsEnter {
 	public static void main(String[] args) {
+		// obtenim les dades
+		System.out.println("Introdueix texts (enter sol per finalitzar)");
+		String text = Entrada.readLine();
+		// bucle while
+		while (!text.isEmpty()) {
+			System.out.println("Estricte?");
+			String respostaEstricte = Entrada.readLine();
+		       	boolean estricte = UtilString.respostaABoolean(respostaEstricte);
+			boolean esEnter = UtilString.esEnter(text, estricte);
+			if (esEnter) {
+				System.out.println("És enter");
+			} else {
+				System.out.println("No és enter");
+			}
+			int aEnter = UtilString.aEnter(text, estricte);
+			text = Entrada.readLine();
+		}
+		System.out.println("Adéu");
+	}
+}
 
---
+++ 32_28_es_enter_flexible/UtilString.java
@@ -0,0 +1,59 @@
+/* Utilitats per l'exercici 28
+ * Utilitat de transformació de resposta String a boolean
+ * Utilitat de saber si un número és enter
+ */
+public class UtilString {
+	public static boolean respostaABoolean(String resposta) {
+		if (null == resposta) {
+			return false;
+		}
+		resposta = resposta.toLowerCase();
+		if (resposta.equals("s") || resposta.equals("y")) {
+            		return true;
+        	}
+        	if (resposta.equals("sí") || resposta.equals("yes")) {
+            		return true;
+        	}
+        	if (resposta.equals("si") || resposta.equals("vale") || resposta.equals("yeah")) {
+		       	return true;
+        	}
+        	return false;
+	}
+	// Funció per saber si és enter o no (estricte/no estricte)
+	public static boolean esEnter(String text, boolean estricte) {
+		if (text.isEmpty()) {
+			return false;
+		} else {
+			if (text.endsWith("-") || text.endsWith("+")) {
+				return false;
+			}
+			for (int i=0; i < text.length(); i++) {
+				if (Character.isLetter(text.charAt(i))) {
+					return false;
+				}
+				if (estricte) {
+					if (Character.isWhitespace(text.charAt(i))) {
+						return false;
+					} else {
+						return true;
+					}
+				}
+			}
+		}
+		return true;
+	}
+	// Transformació d'un String a enter(estricte/no estricte)
+	public static int aEnter(String text, boolean estricte) {
+		int aEnter;
+		if (!estricte) {
+			text = text.strip();
+			aEnter = Integer.parseInt(text);
+		} else {
+			aEnter = Integer.parseInt(text);
+		}
+		return aEnter;
+	}
+}
+
+		
+
