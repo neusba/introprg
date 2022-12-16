c071766fc6157cb09d649230304ae493bb822e54
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-12-16 19:04:38 +0100

    Finalitzat 32_33

---
+++ 32_33_nota_mes_alta_v4/NotaMesAlta.java
@@ -0,0 +1,39 @@
+/* Versió encara més millorada del exercici anterior
+ * Aquesta vegada la sortida final només mostrarà les notes que no són més alta però en ordre descendent.
+ * Utilitzarem només les eines conegudes fins ara
+ */
+public class NotaMesAlta {
+	public static void main(String[] args) {
+		System.out.println("Introdueix les notes (-1 per finalitzar)");
+		int nota = Integer.parseInt(Entrada.readLine());
+		int notaMesAlta = nota;
+		int contador = 1;
+		String llistaNotes = "";
+		while (nota > 0) {
+			if (nota >= notaMesAlta) {
+				notaMesAlta = nota;
+			}
+			llistaNotes = UtilString.toString(nota, llistaNotes);
+			nota = Integer.parseInt(Entrada.readLine());
+			contador += 1;
+		}
+		mostraSortides(contador, llistaNotes, notaMesAlta);
+	}
+	// Mòdul per mostrar les sortides del programa
+	public static void mostraSortides(int contador, String llistaNotes, int notaMesAlta) {
+		if (contador < 3) {
+			System.out.println("Com a mínim calen dues notes");
+		} else {
+			String llistaFiltrada = UtilString.filtraNotes(llistaNotes, notaMesAlta);
+			String llistaFiltradaDescendent = UtilString.llistaDescendent(llistaFiltrada);
+			System.out.printf("La nota més alta és %d. ", notaMesAlta);
+			if (llistaFiltrada.isEmpty()) {
+				System.out.print("No queda cap altra nota.");
+			} else {
+				String llistaFiltradaOrdenada = UtilString.ordenaNotes(llistaFiltradaDescendent);
+				System.out.printf("La resta de notes és: %s", llistaFiltradaOrdenada);
+			}
+		}
+	}
+}
+
---
+++ 32_33_nota_mes_alta_v4/UtilString.java
@@ -0,0 +1,72 @@
+/* Funcions per l'exercici 32_32
+ * La primera converteix un int a un String
+ * La segona filtra les notes i exclou la més alta de la llista
+ * La tercera funció ordena els nombres en ordre descendent.
+ * La quarta funció els afegeix les comes i connector necessaris per formar la frase coherent.
+ */
+public class UtilString {
+	// Funció que converteix un int a enter
+	public static String toString(int nota, String llistaNotes) {
+		String notaString = Integer.toString(nota);
+		llistaNotes = llistaNotes + notaString + ",";
+		return llistaNotes;
+	}
+	// Funció que filtra les notes per excluir la més alta de la llista anterior
+	public static String filtraNotes(String llistaNotes, int notaMesAlta) {
+		String llistaFiltrada = "";
+		String notaAlta = Integer.toString(notaMesAlta);
+		String temp = "";
+		for (int i=0; i < llistaNotes.length(); i++) {
+			if (!Character.isDigit(llistaNotes.charAt(i))) {
+				if (!temp.equals(notaAlta)) {
+					llistaFiltrada = llistaFiltrada + temp;
+				}
+				temp = "";
+				continue;
+			} else {
+				temp = temp + llistaNotes.charAt(i);
+			}
+		}
+		return llistaFiltrada;
+	}
+	//Funció que exclou els nombre repetits la llista de notes filtrada
+	public static String noRepetits(String llistaFiltrada) {
+		String llistaNoRepetits = "";
+		for (int i=0; i < llistaFiltrada.length(); i++) {
+			for (int j=0; j < llistaNoRepetits.length(); j++) {
+				if (llistaFiltrada.charAt(i) == llistaNoRepetits.charAt(j)) {
+					llistaNoRepetits = llistaNoRepetits + llistaFiltrada.charAt(i);
+				}
+			}
+		}
+		return llistaNoRepetits;
+	}
+
+	//Funció que agafarà la llista final i ordenarà els nombres de manera descendent
+	public static String llistaDescendent(String llistaFiltrada) {
+		String llistaFiltradaDescendent = "";
+		for (int i=10; i >= 1; i++) {
+			for (int j=0; j < llistaFiltrada.length(); j++) {
+				if (i == llistaFiltrada.charAt(j)) {
+					llistaFiltradaDescendent = llistaFiltradaDescendent + llistaFiltrada.charAt(j);
+				}
+			}
+		}
+		return llistaFiltradaDescendent;
+	}	
+	// Funció que ordena la llista final amb les notes que no són la més alta
+	public static String ordenaNotes(String llistaFiltradaDescendent) {
+		String llistaFiltradaOrdenada = "";
+		for (int i=0; i < llistaFiltradaDescendent.length(); i++) {
+			if (i == llistaFiltradaDescendent.length() - 2) {
+				llistaFiltradaOrdenada = llistaFiltradaOrdenada + llistaFiltradaDescendent.charAt(i) + " i ";
+			} else if (i == llistaFiltradaDescendent.length() - 1) {
+				llistaFiltradaOrdenada = llistaFiltradaOrdenada + llistaFiltradaDescendent.charAt(i);
+			} else {
+				llistaFiltradaOrdenada = llistaFiltradaOrdenada + llistaFiltradaDescendent.charAt(i) + ", ";
+			}
+		}
+		return llistaFiltradaOrdenada;
+	}
+}
+
