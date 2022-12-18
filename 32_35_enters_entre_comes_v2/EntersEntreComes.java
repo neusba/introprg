f8fc685f39318f96e21e000b69748216401b88b6
Neus <cf22neus.bravo@iesjoandaustria.org>
2022-12-18 19:20:22 +0100

    finalitzat 32_35

---
+++ 32_35_enters_entre_comes_v2/EntersEntreComes.java
@@ -0,0 +1,22 @@
+/* Versió més completa del exercici anterior
+ * Aquesta vegada els valors de l'array el determinarà l'usuari, suposarem que els valors d'entrada són sempre enters vàlids
+ * L'array tindrà 5 valors
+ */
+public class EntersEntreComes {
+	public static void main(String[] args) {
+		int[] numeros = new int[5];
+		int numeroValor = 1;
+		for (int i=0; i<numeros.length; i++) {
+			System.out.printf("Valor %d?%n", numeroValor);
+			numeros[i] = Integer.parseInt(Entrada.readLine());
+			numeroValor += 1;
+		}
+		for (int i=0; i<numeros.length; i++) {
+			if (i == numeros.length - 1) {
+				System.out.print(numeros[i]);
+			} else {
+				System.out.print(numeros[i] + ", ");
+			}
+		}
+	}
+}
