e23d986d75550ff4beb89fbe4c56c0ac87bd8b8d
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-12-20 20:48:11 +0100

    Finalitzat 32_37  ∾ prgtest autocommit 32_37 #4

--- 32_37_enters_entre_comes_v4/UtilString.java
+++ 32_37_enters_entre_comes_v4/UtilString.java
@@ -27,8 +27,13 @@ public class UtilString {
 	public static char demanaSeparador() {
 		System.out.println("Separador?");
 		String cadena = Entrada.readLine();
-		char separador = cadena.charAt(0);
-		return separador;
+		if (cadena.isEmpty()) {
+			char separador = ',';
+			return separador;
+		} else {
+			char separador = cadena.charAt(0);
+			return separador;
+		}
 	}
 	// funció que separa els valors de la seqüència amb el separador indicat i els mostra per pantalla
 	public static String mostraSequencia(int[] numeros, char separador) {
