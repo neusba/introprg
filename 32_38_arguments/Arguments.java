8cf11f99a7aa114f453c8834a06b9a5714864014
prgtest <cf22neus.bravo@iesjoandaustria.org>
2022-12-25 11:29:05 +0100

    Finalitzat 32_38  ∾ prgtest autocommit 32_38 #2

--- 32_38_arguments/Arguments.java
+++ 32_38_arguments/Arguments.java
@@ -2,16 +2,16 @@
  */
 public class Arguments {
 	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Cap argument");
+			return;
+		}
 		for (int i=0; i<args.length; i++) {
-			if (args.length == 0) {
-				System.out.println("Cap argument");
+			boolean esEnter = UtilString.esEnter(args[i]);
+			if (esEnter) {
+				System.out.printf("[%d] \"%s\": és enter%n", i, args[i]);
 			} else {
-				boolean esEnter = UtilString.esEnter(args[i]);
-				if (esEnter) {
-					System.out.printf("[%d] \"%s\": és enter%n", i, args[i]);
-				} else {
-					System.out.printf("[%d] \"%s\": no és enter%n", i, args[i]);
-				}
+				System.out.printf("[%d] \"%s\": no és enter%n", i, args[i]);
 			}
 		}
 	}
