6c6afefb6c797fb95d7573824ea36ff417b77bcd
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-01-13 20:38:47 +0100

    Finalitzat 32_46

---
+++ 32_46_int2str/TaulaEnterAString.java
@@ -0,0 +1,67 @@
+/* Programa que recrea una taula amb les dues dimension que es donaran per línia de comandes i la mostra en forma de String amb un nombre en cada posició
+ * S'haurà de comvaprovar si els arguments donats són vàlids per continuar amb el programa
+ */
+public class TaulaEnterAString {
+	public static void main(String[] args){
+ 		for (int i=0; i<args.length; i++) {
+ 	        	System.out.println(args[i]);
+			boolean correcte = especificacioCorrecte(args[i]);
+			if (!correcte) {
+				System.out.println("Especificació no vàlida");
+				return;
+			}
+ 	            	int files = obteFiles(args[i]);
+ 	            	int columnes = obteColumnes(args[i]);
+ 	           	if (files < 1 || columnes < 1) {
+ 	                	System.out.println("Especificació no vàlida");
+	            	} else {
+	                	int[][] taula  = new int[files][columnes];
+              			UtilTaula.inicialitzaTaula(taula, 1);
+               			String resultat = UtilTaula.taulaToString(taula);
+                		System.out.println(resultat);
+            		}
+        	}
+    	}
+
+	// Funció que comprova si l'argument donat és correcte
+	public static boolean especificacioCorrecte(String especificacio) {
+		String[] coordenades = especificacio.split("x");
+        	if (coordenades.length != 2) return false;
+        	for (int i=0; i<coordenades.length; i++) {
+            		if (! UtilString.esEnter(coordenades[i])) return false;
+            		int valor = Integer.parseInt(coordenades[i]);
+            		if (valor < 0 || valor > 99) return false;
+        	}
+        	return true;
+    	}
+
+	// funció que que obte les files de la taula a través del primer nombre donat en l'argument de l'usuari
+	public static int obteFiles(String especificacio) {
+		char[] valors = especificacio.toCharArray();
+		String fila = "";
+		for (int i=0; i<valors.length; i++) {
+			if (Character.isDigit(valors[i])) {
+				fila = fila + valors[i];
+			} else {
+				break;
+			}
+		}
+		int files = Integer.parseInt(fila);
+		return files;	
+	}
+
+	// funció que que obte les columnes de la taula a través del segon nombre donat en l'argument de l'usuari
+	public static int obteColumnes(String especificacio) {
+		char[] valors = especificacio.toCharArray();
+		String columna = "";
+		for (int i=valors.length - 1; i>=0; i--) {
+			if (Character.isDigit(valors[i])) {
+				columna = valors[i] + columna;
+			} else {
+				break;
+			}
+		}
+		int columnes = Integer.parseInt(columna);
+		return columnes;
+	}
+}
---
+++ 32_46_int2str/UtilTaula.java
@@ -0,0 +1,24 @@
+/* Funcions per taules de l'exercici 32_46 */
+
+public class UtilTaula {
+	// funcio que retorna la taula inicialitzada
+	public static void inicialitzaTaula(int[][] taula, int valor) {
+		for (int i=0; i<taula.length; i++) {
+			for (int j=0; j<taula[i].length; j++) {
+				taula[i][j] = valor;
+			}
+		}
+	}
+
+	// Funcio que retorna la taula inicial en forma de String transformada
+	public static String taulaToString(int[][] taula) {
+		String resultat = "";
+		for (int i=0; i<taula.length; i++) {
+			for (int j=0; j<taula[i].length; j++) {
+				resultat = resultat + String.format("%8d", taula[i][j]);
+			}
+			resultat = resultat + String.format("%n");
+		}
+		return resultat;
+	}
+}
