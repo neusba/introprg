b35c9aa2eddbc0c849d1bd20134ce78c58d0fa7b
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-01-13 21:39:56 +0100

    Finalitzat 32_47  ∾ prgtest autocommit 32_47 #6

--- 32_47_inisequencial/UtilTaula.java
+++ 32_47_inisequencial/UtilTaula.java
@@ -16,7 +16,11 @@ public class UtilTaula {
 		for (int i=0; i<taula.length; i++) {
 			for (int j=0; j<taula[i].length; j++) {
 				sequenciador += 1;
-				taula[i][j] = valorInicial;
+				if (i == 0 && j == 0) {
+					taula[i][j] = valorInicial;
+				} else {
+					taula[i][j] = valorInicial + sequenciador;
+				}
 			}
 		}
 	}
