b5b3e720b4eed7926dc6e94da4b992f7b9df9aed
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-01-18 20:34:20 +0100

    Finalitzat 32_53

---
+++ 32_53_preordre/UtilString.java
@@ -0,0 +1,23 @@
+/* Funcions per l'exercici 32_53
+ * La primera funció retornarà un string amb només les lletres del text donat
+ */
+
+public class UtilString {
+	// nomes lletres recursiu
+	public static String nomesLletres(String text) {
+		String textLletres = "";
+		if (text.isEmpty()) return "";				// cas base
+		
+		char primer = text.charAt(0);
+		String esLletra = "";
+		if (Character.isLetter(primer)) {			// tracta pas actual
+			esLletra = esLletra + primer;
+		}
+
+		text = text.substring(1);
+		String restaText = nomesLletres(text);			// pas recursiu
+		
+		textLletres = esLletra + restaText;
+		return textLletres;
+	}
+}	
