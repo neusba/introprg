a5ae6ed69ddbb592902ae1dce3c119b843620d53
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-01-31 01:08:46 +0100

    finalitzat 33_06

---
+++ 33_06_traduccio/Traduccio.java
@@ -0,0 +1,56 @@
+/* Programa que des d'un fitxer origen reemplaçarà les paraules indicades a un fitxer traducció i 
+ * escriurà amb el text intercanviat a un nou fitxer anomenat destinació
+ */
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Traduccio {
+	// variables globales
+	public static void main(String[] args) throws IOException {
+		if (args.length != 3) {
+			System.out.println("ERROR: Cal especificar els fitxers origen, traducció i destinació");
+		}
+		String fitxerOrigen = args[0];
+		String fitxerTraduccio = args[1];
+		String fitxerDestinacio = args[2];
+		tradueix(fitxerOrigen, fitxerTraduccio, fitxerDestinacio);
+	}
+
+	public static void tradueix(String fitxerOrigen, String fitxerTraduccio, String fitxerDestinacio) throws IOException {
+		BufferedReader input = new BufferedReader(new FileReader(fitxerOrigen));
+		BufferedWriter output = new BufferedWriter(new FileWriter(fitxerDestinacio));
+		while (true) {
+			String liniaOrigen = input.readLine();
+			if (liniaOrigen == null) { break; }
+			liniaOrigen = tradueixLinia(liniaOrigen, fitxerTraduccio);
+			output.write(String.format("%s%n", liniaOrigen));
+		}
+		input.close();
+		output.close();
+	}
+
+	public static String tradueixLinia(String linia, String fitxerTraduccio) throws IOException {
+		BufferedReader input = new BufferedReader(new FileReader(fitxerTraduccio));
+		while (true) {
+			String traduccio = input.readLine(); // _,_
+			if (traduccio == null) { break;}
+			String[] clauValor = traduccio.split(",");
+			linia = linia.replace(clauValor[0], clauValor[1]);
+		}
+		input.close();
+		return linia;	
+	}
+}	
+
+			
+
+
+			
+
+
+
+
+
