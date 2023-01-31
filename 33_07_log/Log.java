f8a408e6861136f96c1d20bac65c34b92e2435bf
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-01-31 03:13:10 +0100

    Biblioteca Registres Log  ∾ prgtest autocommit 33_07 #16

--- 33_07_log/Log.java
+++ 33_07_log/Log.java
@@ -10,7 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Log {
-	public static int sequencial;
+	public static int sequencial = 1;
 	// Per indicar errors greus, sovint irrecuperables
 	public static String printError(String text) throws IOException {
 		String type = "ERROR:";
