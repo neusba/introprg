f2b6d09f56b98aca9d81195981771700256407ed
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-02-02 23:17:40 +0100

    Proba 1 33_10  ∾ prgtest autocommit 33_10 #12

--- 33_10_inspecciona/Inspecciona.java
+++ 33_10_inspecciona/Inspecciona.java
@@ -78,11 +78,11 @@ public class Inspecciona {
         } else {
             BufferedReader input = new BufferedReader(new FileReader(path));
             System.out.printf("%s fitxer de mida en bytes: %d%n", permisos, mida);
-            System.out.println("Amb els continguts:");
             while (true) {
                 String linia = input.readLine();
                 if (linia == null) { break; }
                 if (linia.isEmpty()) { continue; }
+                //System.out.println("Amb els continguts:");
                 System.out.printf("|%s|%n", linia);
             }
             input.close();
