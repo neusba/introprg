aefb3338124415b8b3423354de1edd2da9b5db26
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-02-02 16:18:37 +0100

    Agefit fitxer desconegut  ∾ prgtest autocommit 33_08 #13

--- 33_08_classificador/ClassificaMatricules.java
+++ 33_08_classificador/ClassificaMatricules.java
@@ -9,53 +9,49 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class ClassificaMatricules {
-	public static void main(String[] args) throws IOException {
-		String path = "llegides.txt";
-                String pathItalianes = "italianes.txt";
-                String pathDesconegudes = "desconegudes.txt";
+    public static void main(String[] args) throws IOException {
+        String path = "llegides.txt";
+        String pathItalianes = "italianes.txt";
+        String pathDesconegudes = "desconegudes.txt";
+        BufferedReader input = new BufferedReader(new FileReader(path));
+        BufferedWriter outputItalianes = new BufferedWriter(new FileWriter(pathItalianes));
+        BufferedWriter outputDesconegudes = new BufferedWriter(new FileWriter(pathDesconegudes));
 
-                BufferedReader input = new BufferedReader(new FileReader(path));
-                BufferedWriter outputItalianes = new BufferedWriter(new FileWriter(pathItalianes));
-                BufferedWriter outputDesconegudes = new BufferedWriter(new FileWriter(pathDesconegudes));
-                while (true) {
-                    String linia = input.readLine();
-                    if (linia == null) { break; }
-                    if (linia.isEmpty()) { continue; }
-                    linia = linia.strip();
-                    boolean italiana = matriculaValida(linia);
-                    if (italiana) {
-                        outputItalianes.write(String.format("%s%n", linia));
-                    } else {
-                        outputDesconegudes.write(String.format("%s%n", linia));
+        while (true) {
+            String linia = input.readLine();
+            if (linia == null) { break; }
+            if (linia.isEmpty()) { continue; }
+            linia = linia.strip();
+            boolean valida = matriculaItalianaValida(linia);
+            if (valida) {
+                outputItalianes.write(String.format("%s%n", linia));
+            } else {
+                outputDesconegudes.write(String.format("%s%n", linia));
+            }
+        }
+        input.close();
+        outputItalianes.close();
+        outputDesconegudes.close();
+    }
+
+    // Funcio que retorna true si la matrícula és vàlida com a italiana o no
+    public static boolean matriculaItalianaValida(String matricula) {
+        String noValides = "IOQU";
+        if (matricula.length() != 7) {
+            return false;
+        }
+        for (int i=0; i < matricula.length(); i++) {
+            char lletra = matricula.charAt(i);
+                if (i > 1 && i < 5) {
+                    if (!Character.isDigit(lletra)) { return false; }
+                } else {
+                    if (!Character.isLetter(lletra)) { return false; }
+                    for (int j=0; j < noValides.length(); j++) {
+                        if (lletra == noValides.charAt(j)) { return false; }
                     }
                 }
-                input.close();
-                outputItalianes.close();
-                outputDesconegudes.close();
         }
-
-	public static boolean matriculaValida(String text) throws IOException {
-		String noValides = "IOQU";
-		if (text.length() != 7) {
-			return false;
-		}
-		for (int i=0; i < text.length(); i++) {
-			char lletra = text.charAt(i);
-			if (i > 1 && i < 5) {
-				if (!Character.isDigit(lletra)) {
-					return false;
-				}
-			} else if (Character.isLetter(lletra) && Character.isUpperCase(lletra)) {
-                            if (lletra >= 'A' && lletra <= 'Z') {
-                                for (int j=0; j < noValides.length(); j++) {
-                                    if (lletra == noValides.charAt(j)) {
-                                        return false;
-                                    }
-                                }
-                                return true;
-                            }
-                    }
-		}
-		return true;
-	}
+        return true;
+    }
 }
+
