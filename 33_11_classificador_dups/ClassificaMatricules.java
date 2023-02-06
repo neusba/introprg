18e4ba535cfb2a5d134cb62141c7187c8e1cd925
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-02-07 00:32:34 +0100

    Array eliminat, canvi en el format de comprovació de repetides  ∾ prgtest autocommit 33_11 #29

--- 33_11_classificador_dups/ClassificaMatricules.java
+++ 33_11_classificador_dups/ClassificaMatricules.java
@@ -21,12 +21,14 @@ public class ClassificaMatricules {
             if (linia == null) break;
             linia = linia.strip();
             if (!matriculaItalianaValida(linia)) {
+                System.out.println(linia);
                 if (!exists(linia, "desconegudes.txt")) {
                     BufferedWriter noValid = new BufferedWriter(new FileWriter("desconegudes.txt", true));
                     noValid.write(String.format("%s%n", linia));
                     noValid.close();
                 }
             } else {
+                System.out.println(linia);
                 if (!exists(linia, "italianes.txt")) {
                     BufferedWriter valid = new BufferedWriter(new FileWriter("italianes.txt", true));
                     valid.write(String.format("%s%n", linia));
