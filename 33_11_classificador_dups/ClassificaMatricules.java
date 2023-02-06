7a7919f83f052d0830736f4301d5e9df5abe1eeb
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-02-06 23:33:54 +0100

    Array eliminat, canvi en el format de comprovació de repetides  ∾ prgtest autocommit 33_11 #8

--- 33_11_classificador_dups/ClassificaMatricules.java
+++ 33_11_classificador_dups/ClassificaMatricules.java
@@ -1,6 +1,6 @@
 /* Programa que llegirà un fitxer amb un registre de matrícules i les classificarà en dos fitxers nous:
  * italianes.txt i desconegudes.txt
- * S'haurà de comprovar si la matrícula és italiana o no
+ * S'haurà de comprovar si la matrícula està repetida o no
  */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -14,51 +14,30 @@ public class ClassificaMatricules {
     public static String pathDesconegudes = "desconegudes.txt";
     
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new FileReader(path));
-        BufferedReader inputItalianes = new BufferedReader(new FileReader(pathItalianes));
-        BufferedReader inputDesconegudes = new BufferedReader(new FileReader(pathDesconegudes));
-        BufferedWriter outputItalianes = new BufferedWriter(new FileWriter(pathItalianes));
-        BufferedWriter outputDesconegudes = new BufferedWriter(new FileWriter(pathDesconegudes));
+        BufferedReader input = new BufferedReader(new FileReader(path));                                // Lectura del fitxer llegides
+        BufferedWriter outputItalianes = new BufferedWriter(new FileWriter(pathItalianes));             // Escritura del fitxer italianes
+        BufferedWriter outputDesconegudes = new BufferedWriter(new FileWriter(pathDesconegudes));       // Escritura del fitxer Desconegudes
+        
 
         while (true) {
             String linia = input.readLine();
             if (linia == null) { break; }
             if (linia.isEmpty()) { continue; }
             linia = linia.strip();
-            boolean valida = matriculaItalianaValida(linia);
-            if (valida) {
-                int comptaLinies = 0;
-                while (true) {
-                    String checkLinia = inputItalianes.readLine();
-                    comptaLinies += 1;
-                    if (checkLinia == null) { break; }
-                    if (checkLinia.equals(linia)) {
-                        outputItalianes.write(String.format("%s%n", linia));
-                    }
-                }
-                if (comptaLinies == 0) {
+            if (matriculaItalianaValida(linia)) {                                                       // Comprova si és una matrícula vàlida
+                if (!exists(linia, "italianes.txt")) {
                     outputItalianes.write(String.format("%s%n", linia));
+                    outputItalianes.close();
                 }
             } else {
-                int comptaLinies = 0;
-                while (true) {
-                    String checkLinia = inputDesconegudes.readLine();
-                    comptaLinies += 1;
-                    if (checkLinia == null) { break; }
-                    if (checkLinia.equals(linia)) {
-                        outputDesconegudes.write(String.format("%s%n", linia));
-                    }
-                }
-                if (comptaLinies == 0) {
+                if (!exists(linia, "desconegudes.txt")) {
                     outputDesconegudes.write(String.format("%s%n", linia));
+                    outputDesconegudes.close();
                 }
             }
         }
         input.close();
-        outputDesconegudes.close();
-        outputItalianes.close();
-    }
-
+    } 
     // Funcio que retorna true si la matrícula és vàlida com a italiana o no
     public static boolean matriculaItalianaValida(String matricula) {
         String noValides = "IOQU";
@@ -67,16 +46,31 @@ public class ClassificaMatricules {
         }
         for (int i=0; i < matricula.length(); i++) {
             char lletra = matricula.charAt(i);
-            if (i > 1 && i < 5) {
-                if (!Character.isDigit(lletra)) { return false; }
-            } else {
-                if (!Character.isLetter(lletra)) { return false; }
-                if (Character.isLowerCase(lletra)) { return false; }
-                for (int j=0; j < noValides.length(); j++) {
-                    if (lletra == noValides.charAt(j)) { return false; }
+                if (i > 1 && i < 5) {
+                    if (!Character.isDigit(lletra)) { return false; }
+                } else {
+                    if (!Character.isLetter(lletra)) { return false; }
+                    if (Character.isLowerCase(lletra)) { return false; }
+                    for (int j=0; j < noValides.length(); j++) {
+                        if (lletra == noValides.charAt(j)) { return false; }
+                    }
                 }
-            }
         }
         return true;
     }
+    // Procediment que comprova si la matrícula està repetida dins del seu fitxer corresponent
+    public static boolean exists(String linia, String path) throws IOException {
+        BufferedReader input = new BufferedReader(new FileReader(path));
+        while (true) {
+            String check = input.readLine();
+            if (check == null) { break; }
+            if (linia.equals(check)) {
+                input.close();
+                return true;
+            }
+        }
+        input.close();
+        return false;
+    }
 }
+
