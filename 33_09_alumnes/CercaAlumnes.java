312256f27a2ac638694bae42841eb4698430112d
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-02-07 16:38:23 +0100

    Finalitzat 33_09

--- 33_09_alumnes/CercaAlumnes.java
+++ 33_09_alumnes/CercaAlumnes.java
@@ -12,44 +12,23 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class CercaAlumnes {
-    public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
-
-        // assegura que hi ha el criteri de cerca
-        // XXX a completar
-
-        String path = " alumnes.csv2";                                          // declaracions, inicialitzacions, apertura de fitxer, ignora línia de capçaleres, etc.
-        BufferedReader input = new BufferedReader(new FileReader(path));
-        while (true) {
-            String linia = input.readLine();                                    // llegeix entrada i finalitza bucle si no en queden més
-            if (linia == null) { break; }
-            if (linia.contains("nom,email")) { continue; }
-            Alumne linia = new Alumne();                                        // converteix l'entrada a Alumne
-            
-
-            // comprova si el criteri de cerca es troba dins del nom o
-            // el email. Si és així, mostra'l
-            // XXX a completar
-        }
-        input.close();                                                          // consideracions finals com ara el tancament del fitxer
+    static class Alumne {
+            String nom;
+            String email;
+            int edat;
+            boolean esOient;
+            int[] notes;
     }
-        // Clase Alumne
-        static class Alumne {
-        String nom;
-        String email;
-        int edat;
-        boolean esOient;
-        int[] notes;
 
     public static Alumne construeixAlumne(String nom, String email, int edat, boolean esOient, int[] notes) {
-        Alumne alumne = new Alumne();
-        alumne.nom = nom;
-        alumne.email = email;
-        alumne.edat = edat;
-        alumne.esOient = esOient;
-        alumne.notes = notes;
-        return alumne;
-    }
+            Alumne alumne = new Alumne();
+            alumne.nom = nom;
+            alumne.email = email;
+            alumne.edat = edat;
+            alumne.esOient = esOient;
+            alumne.notes = notes;
+            return alumne;
+        }
 
     public static void mostraAlumne(Alumne alumne) {
         System.out.println("Alumne: " + alumne.nom);
@@ -66,18 +45,58 @@ public class CercaAlumnes {
                 alumne.nom, alumne.email, alumne.edat, alumne.esOient,
                 notesACsv(alumne.notes));
     }
+
     // converteix un array de notes a CSV
     // Té en comptes els valors NP com a -1
     public static String notesACsv(int[] notes) {
-        
+        String notesCsv = "";
+        for (int i=0; i<notes.length; i++) {
+            if (i == notes.length - 1) {
+                notesCsv = notesCsv + notes[i];
+            } else {
+                notesCsv = notesCsv + notes[i] + ",";
+            }
+        }
+        notesCsv = notesCsv.replace("-1","NP");
+        return notesCsv;
     }
 
     public static String alumneACsv(Alumne alumne) {
-        // XXX a completar encara que no es fa servir en aquest programa
+        String alumneCsv = String.format("%s,%s,%d,%b,%s", alumne.nom, alumne.email, alumne.edat, alumne.esOient, notesACsv(alumne.notes));
+        return alumneCsv;
     }
 
     public static Alumne csvAAlumne(String csv) {
-        // XXX a completar
+        String[] dades = csv.split(",", 5);
+        String[] notesCsv = dades[4].split(",");
+        String nom = dades[0];
+        String email = dades[1];
+        int edat = Integer.parseInt(dades[2]);
+        boolean esOient = dades[3].equals("true")?true:false;
+        int[] notes = new int[6];
+        for (int i=0; i<notesCsv.length; i++) {
+            notes[i] = Integer.parseInt(notesCsv[i]);
+        }
+        Alumne alumne = construeixAlumne(nom, email, edat, esOient, notes);
+        return alumne;
     }
-
+    public static void main(String[] args) throws IOException {
+        // assegura que hi ha el criteri de cerca
+        if (args.length == 0) {
+            System.out.println("No hi ha cap argument");
+            return;
+        }
+        String path = "alumnes.csv";
+        BufferedReader input = new BufferedReader(new FileReader(path));
+        while (true) {
+            String linia = input.readLine();
+            if (linia == null) break;
+            Alumne alumne = csvAAlumne(linia);
+            if (alumne.nom.contains(args[0]) || alumne.email.contains(args[0])) {
+                mostraAlumne(alumne);
+            }
+        }
+        input.close();
+    }
+    
 }
