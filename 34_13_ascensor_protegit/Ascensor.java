05ac2b8c376022ccf7b09c371a0574fb6a8b37f7
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-02-09 02:39:03 +0100

    Finalitzat 34_13

---
+++ 34_13_ascensor_protegit/Ascensor.java
@@ -0,0 +1,29 @@
+/* Fitxer que conté la clase Ascensor amb les seves propietats
+ * Afegim Getters i Setters
+ */
+
+public class Ascensor {
+    private int pis = -1;
+    private String moviment = "aturat";
+    
+    // Getters
+    public int getPis() {
+        return pis;
+    }
+    public String getMoviment() {
+        return moviment;
+    }
+    // Setters
+    public void setPis(int nouPis) {
+        if (nouPis > -2 && nouPis < 11) {
+            pis = nouPis;
+        }
+    }
+    public void setMoviment(String nouMoviment) {
+        switch (nouMoviment) {
+            case "aturat": moviment = nouMoviment; break;
+            case "pujant": moviment = nouMoviment; break;
+            case "baixant": moviment = nouMoviment; break;
+        }
+    }
+}
---
+++ 34_13_ascensor_protegit/UsaAscensor.java
@@ -0,0 +1,18 @@
+/* Fitxer del programa Ascensor protegit que conté el main amb la creació de la instància
+ * i totes les crides i missatges que es veuran per pantalla
+ */
+
+public class UsaAscensor {
+    public static void main(String[] args) {
+        Ascensor ascensor = new Ascensor();
+
+        System.out.println("Pis inicial: " + ascensor.getPis());
+        System.out.println("Moviment inicial: " + ascensor.getMoviment());
+        System.out.println("Introdueix nou pis:");
+        ascensor.setPis(Integer.parseInt(Entrada.readLine()));
+        System.out.println("Introdueix nou moviment:");
+        ascensor.setMoviment(Entrada.readLine());
+        System.out.println("Pis final: " + ascensor.getPis());
+        System.out.println("Moviment final: " + ascensor.getMoviment());
+    }
+}
