6ee41ae5610a213c2f0f4fe2a27aa3f2e3911b91
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-02-09 11:30:21 +0100

    Finalitzat 34_13  ∾ prgtest autocommit 34_13 #5

--- 34_13_ascensor_protegit/UsaAscensor.java
+++ 34_13_ascensor_protegit/UsaAscensor.java
@@ -9,7 +9,18 @@ public class UsaAscensor {
         System.out.println("Pis inicial: " + ascensor.getPis());
         System.out.println("Moviment inicial: " + ascensor.getMoviment());
         System.out.println("Introdueix nou pis:");
-        ascensor.setPis(Integer.parseInt(Entrada.readLine()));
+        String nouPis = Entrada.readLine();
+        int numPis;
+        if (nouPis.length() != 1) {
+            numPis = ascensor.getPis();
+        }
+        for (int i=0; i<nouPis.length(); i++) {
+            if (!Character.isDigit(nouPis.charAt(i))) {
+                numPis = ascensor.getPis();
+            }
+        }
+        numPis = Integer.parseInt(nouPis);
+        ascensor.setPis(numPis);          // ARRELGAR #########
         System.out.println("Introdueix nou moviment:");
         ascensor.setMoviment(Entrada.readLine());
         System.out.println("Pis final: " + ascensor.getPis());
