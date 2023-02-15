7e5d9fb20075ef0ade2697077c4ed2238b204474
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-02-15 14:54:53 +0100

    finalitzat 34_15

--- 34_15_ascensor_informatiu/UsaAscensor.java
+++ 34_15_ascensor_informatiu/UsaAscensor.java
@@ -3,37 +3,19 @@
  * Farem l'ús de propietats privades, accessors i mètodes per coneixer l'estat del ascensor
  */
 public class UsaAscensor {
-    public static int llegeixEnter() {
-        String resposta = Entrada.readLine();
-        if (! UtilString.esEnter(resposta)) {
-            return -2;
-        }
-        return Integer.parseInt(resposta);
-    }
     public static void main(String[] args) {
         Ascensor ascensor = new Ascensor();
-
-        System.out.println("Pis inicial");
-        ascensor.setPis(llegeixEnter());
-        System.out.printf("Inicialment l'ascensor està %s%n", ascensor.XXX);
-
-        // aturat a pujant
-        boolean resposta = ascensor.arrencaAmunt();
-        System.out.printf("En arrencar amunt l'ascensor respon %b i està %s%n",
-            resposta, ascensor.XXX);
-
-        // demanem que baixi sense aturar
-        resposta = ascensor.arrencaAbaix();
-        System.out.printf("En arrencar a baix l'ascensor respon %b i està %s%n",
-            resposta, ascensor.XXX);
-
-        // passem del pis indicat al la planta baixa
-        System.out.println("Anant a la planta baixa quan està " + ascensor.comEsta());
-        while (! ascensor.estaAbaix()) {
-            ascensor.XXX;       // passa al següent pis
-            System.out.println("\t ara està "+ ascensor.XXX);
+        System.out.println("Pis inicial: " + ascensor.getPis());
+        System.out.println("Moviment inicial: " + ascensor.getMoviment());
+        System.out.println("Introdueix nou pis:");
+        String nouPis = Entrada.readLine();
+        if (UtilString.esEnter(nouPis)) {
+            ascensor.setPis(Integer.parseInt(nouPis));
         }
-        ascensor.aturat();
-        System.out.printf("Està %s. Final de trajecte!%n", ascensor.comEsta());
+        System.out.println("Introdueix nou moviment:");
+        ascensor.setMoviment(Entrada.readLine());
+        System.out.println("Pis final: " + ascensor.getPis());
+        System.out.println("Moviment final: " + ascensor.getMoviment());
+        System.out.println("Estat de l'ascensor: " + ascensor.comEsta());
     }
 }
