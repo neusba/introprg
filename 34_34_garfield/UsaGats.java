842dedfffe164c86182c2f199dac695ee77a0a22
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-03-18 12:50:16 +0100

    Finalitzat 34_34  ∾ prgtest autocommit 34_34 #4

--- 34_34_garfield/UsaGats.java
+++ 34_34_garfield/UsaGats.java
@@ -1,24 +1,5 @@
 /* Programa que prova el polimorfisme de les classes amb Gats */
 
-// CLASSE PÚBLICA
-public class UsaGats {
-    public static void main(String[] args) {
-        Gat[] gats = new Gat[] {
-            new Gat("Misifú"),
-            new GatRenat(),
-            new Garfield()
-        };
-        for (Gat gat: gats) {
-            System.out.println("Entrenant el gat " + gat.getNom());
-            System.out.println(gat.getNom() + " diu: "+ gat.seu());
-            System.out.println(gat.getNom() + " diu: "+ gat.aixecat());
-            System.out.println(gat.getNom() + " diu: "+ gat.aixecat());
-            System.out.println(gat.getNom() + " diu: "+ gat.estirat());
-            System.out.println();
-        }
-    }
-}
-
 // CLASSE GAT
 class Gat {
     private final String nom;
@@ -179,3 +160,22 @@ class Garfield extends Gat {
         return("no faig res");
     }
 }
+// CLASSE PÚBLICA
+public class UsaGats {
+    public static void main(String[] args) {
+        Gat[] gats = new Gat[] {
+            new Gat("Misifú"),
+            new GatRenat(),
+            new Garfield()
+        };
+        for (Gat gat: gats) {
+            System.out.println("Entrenant el gat " + gat.getNom());
+            System.out.println(gat.getNom() + " diu: "+ gat.seu());
+            System.out.println(gat.getNom() + " diu: "+ gat.aixecat());
+            System.out.println(gat.getNom() + " diu: "+ gat.aixecat());
+            System.out.println(gat.getNom() + " diu: "+ gat.estirat());
+            System.out.println();
+        }
+    }
+}
+
