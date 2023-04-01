1443eea3a68facedc4fd2f203dc5c26c538a6160
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-04-01 13:39:33 +0200

    Finalitzat 35_01  ∾ prgtest autocommit 35_01 #5

--- 35_01_vi/Vi.java
+++ 35_01_vi/Vi.java
@@ -65,7 +65,7 @@ class Vi {
             return false;
         }
         if (this.preu == -1 || this.estoc == -1) { return false; }
-        return false;
+        return true;
     }
     public static String normalitzaNom(String nom) {
         // checkea si el nombre és un nombre válido, si no lo es, devuelve "NOM NO VÀLID"
