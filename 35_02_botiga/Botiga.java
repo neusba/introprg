b393194a84fb9a141379e778ee7dc15a7bfcc828
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-04-02 16:19:48 +0200

    Primer test 35_02 sense afegir mthd normalitzaNom()

---
+++ 35_02_botiga/Botiga.java
@@ -0,0 +1,82 @@
+/* Fitxer que conté la classe Botiga y totes les seves propietats, accessors i mètodes per fer-la funcionar.
+ * Afegirem els vins de la classe vi a la classe Botiga, per tant serà una relació d'agregació.
+ * Inclourem la classe Vi dins de la classe Botiga per tal de poder fer-les interactuar
+ * La funcionalitat més important de la botiga és poder cercar un vi pel seu nom.
+ */
+
+class Botiga {
+    private int DEFAULT_MAX_VINS = 10;                // Nombre màxim de vins que hi caben a la botiga
+    private Vi[] vins;
+
+    // Constructors
+    public Botiga() {
+        this.DEFAULT_MAX_VINS = DEFAULT_MAX_VINS;
+
+    }
+    public Botiga(int maxVins) {
+        if (maxVins < 1) {
+            this.DEFAULT_MAX_VINS = DEFAULT_MAX_VINS;
+        } else {
+            this.DEFAULT_MAX_VINS = maxVins;
+        }
+    }
+
+    // Mètodes
+    // afegeix un vi a la botiga
+    public Vi afegeix(Vi vi) {
+        String nomVi = vi.getNom();
+        // COMPROVA que es VALID
+        if (!vi.esValid()) { return null; }             
+        // si hi ha un altre vi amb el MATEIX NOM RETURNS NULL
+        if (estaABotiga(nomVi)) { return null; }       
+        // AFEGIRÀ el vi a una POSICIO NULL de l'array i INDICARA que s'ha AFEGIT
+        for (int i=0; i<vins.length; i++) {
+            if (vins[i] == null) {
+                vins[i] = vi;
+                return vi;
+            }
+        }
+        return null;    // en cas que la botiga estigui plena
+    }
+    // elimina un vi de la botiga
+    public Vi elimina(String vi) {
+        for (int i=0; i<vins.length; i++) {
+            if (vins[i].getNom().equals(vi)) {
+                // comprova stock
+                if (vins[i].getEstoc() > 0) {
+                    return null;
+                }
+                vins[i] = null;
+                return vins[i];
+            }
+        }
+        return null;
+
+    }
+    // cerca un vi en concret a la botiga
+    public Vi cerca(String nom) {
+        for (int i=0; i<vins.length; i++) {
+            if (vins[i].getNom().equals(nom)) {
+                return vins[i];
+            }
+        }
+        return null;
+    }
+
+    // Mètodes extres
+    // comprova si el vi ja ha sigut afeigit a la botiga
+    public boolean estaABotiga(String nomVi) {
+        // recorre array de vinos disponibles
+        // compara el nombre del vino [i] con el del parametro
+        // Si alguno coincide RETURNS TRUE
+            // Si nunca devuelve true, al acabar el bucle RETURNS FALSE;
+        for (int i=0; i<vins.length; i++) {
+            if (vins[i].getNom().equals(nomVi)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+    
+
