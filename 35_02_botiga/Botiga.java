9d387c975892a694d84d71f8d516bd91be584342
Neus <cf22neus.bravo@iesjoandaustria.org>
2023-04-07 13:14:53 +0200

    Finalitzat 35_02

--- 35_02_botiga/Botiga.java
+++ 35_02_botiga/Botiga.java
@@ -1,39 +1,3 @@
-/* Fitxer que conté la classe Botiga y totes les seves propietats, accessors i mètodes per fer-la funcionar.
- * Afegirem els vins de la classe vi a la classe Botiga, per tant serà una relació d'agregació.
- * Inclourem la classe Vi dins de la classe Botiga per tal de poder fer-les interactuar
- * La funcionalitat més important de la botiga és poder cercar un vi pel seu nom.
- */
-
-class Botiga {
-    private int DEFAULT_MAX_VINS = 10;                // Nombre màxim de vins que hi caben a la botiga
-    private Vi[] vins;
-
-    // Constructors
-    public Botiga() {
-        this.vins = new Vi[DEFAULT_MAX_VINS];
-    }
-       // específic
-    public Botiga(int maxVins) {
-        if (maxVins <= 0) {
-            this. vins = new Vi[DEFAULT_MAX_VINS];
-        } else {
-            this.vins = new Vi[maxVins];
-        }
-    }
-
-    // MÈTODES
-    public Vi afegeix(Vi vi) {
-        // recibo instancia de vino
-        // compruebo si la instància es válida
-        if (!vi.esValid()) { return null; }
-        // Para comprobar que no està repetido el nombre en la tienda
-        // recorro el array de vinos
-        for (int i=0; i<vins.length; i++) {
-            // Si no encuentra ninguna instancia con el mismo nombre la añado
-            if (vins[i] == null) {
-                vins[i] = vi;
-                return vi;
-            }
             // Si encuentra una instància con el mismo nombre
             if (vins[i].getNom().equals(vi.getNom())) {
                // no puede añadirla a la tienda, la devuelve
