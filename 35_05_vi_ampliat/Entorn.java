724ec86a28f63ed0969ddef519a7210b117a4990
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-04-18 20:27:52 +0200

    segon testing  ∾ prgtest autocommit 35_05 #13

--- 35_05_vi_ampliat/Entorn.java
+++ 35_05_vi_ampliat/Entorn.java
@@ -69,22 +69,23 @@ public class Entorn {
         for (int i=0; i<propietats.length; i++) {
             System.out.printf("%s> ", nomVariables[i]);
             propietats[i] = Entrada.readLine();
+            if (nomVariables[i].equals("preu max.")) {
+                if (Character.isLetter(preu.charAt(0))) {
+                    System.out.println("Ha de ser un nombre enter");
+                    i=0;
+                    continue;
+                }
+            }
             if (propietats[i].equals("!")) break;
             if (propietats[i].equals("")) continue;
         } 
         // Convertimos los String a int
         if (preu.equals("") || preu.equals("!")) {
             preu = "-1";
-        } else if (Character.isLetter(preu.charAt(0))) {
-            System.out.println("Ha de ser un nombre enter");
-            return;
         }
         preuInt = Integer.parseInt(preu);
         if (estoc.equals("") || estoc.equals("!") || Character.isLetter(estoc.charAt(0))) {
             estoc = "-1";
-        } else if (Character.isLetter(estoc.charAt(0))) {
-            System.out.println("Ha de ser un nombre enter");
-            return;
         }
         estocInt = Integer.parseInt(estoc);
         // creamos la instancia de vino 
