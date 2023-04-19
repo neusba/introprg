17967869dd0f4de20457316d359c740fbbab0cf3
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-04-19 13:33:01 +0200

    Simplificat  ∾ prgtest autocommit 35_10 #3

--- 35_10_gatposicio/GatPosicio.java
+++ 35_10_gatposicio/GatPosicio.java
@@ -16,7 +16,7 @@ enum GatPosicio {
             GatPosicio valor = GatPosicio.valueOf(posicio);
             return valor;
         } catch (IllegalArgumentException e) {
-           throw new IllegalArgumentException("Valor \"posicio\" no suportat per GatPosicio");
+           throw new IllegalArgumentException("Valor \"content\" no suportat per GatPosicio");
         }
     }
 }
