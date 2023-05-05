38524f788b6310ea0b8a798315499a9cc47ec811
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-05-05 21:01:47 +0200

    Actualització automàtica de l'informe de resultats ∾ prgtest autocommit 52_05 #2

--- 52_05_suma/UsaPunt.java
+++ 52_05_suma/UsaPunt.java
@@ -10,11 +10,20 @@ public class UsaPunt {
         int y2 = 0;
         try {
             x1 = Integer.parseInt(args[0]);
-            y1 = Integer.parseInt(args[1]);
-            x2 = Integer.parseInt(args[2]);
-            y2 = Integer.parseInt(args[3]);
-        } catch (Exception e) {}
-
+        } catch (Exception e) {
+            try {
+                y1 = Integer.parseInt(args[1]);
+            } catch (Exception e1) {
+                try {
+                    x2 = Integer.parseInt(args[2]);
+                } catch(Exception e2) {
+                    try {
+                        y2 = Integer.parseInt(args[3]);
+                    } catch (Exception e3) {
+                    }
+                }
+            }
+        }
         Punt p1 = new Punt(x1, y1);
         Punt p2 = new Punt(x2, y2);
         System.out.printf("p1: (%d, %d)%n", p1.getX(), p1.getY());
