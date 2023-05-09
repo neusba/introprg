421ff2acfe7bf956ad33212437015dce7d91340d
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-05-09 20:25:57 +0200

    Creada classe GestorLite per fer proves bàsiques del codi existent  ∾ prgtest autocommit 52_11 #3

--- 52_11_gestor_lite/GestorLite.java
+++ 52_11_gestor_lite/GestorLite.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public class GestorLite {
     public static void main(String[] args) {
         Client demo = creaClientDemo();
-        Lloguer primer = new Lloguer(new Vehicle("Seat", "600", 1), 2);
-        Lloguer segon = new Lloguer(new Vehicle("Tata", "Vista", 1), 5);
-        Lloguer tercer = new Lloguer(new Vehicle("Fiat", "Uno", 1), 3);
+        demo.getLloguers().add(new Lloguer(new Vehicle("Seat", "600", 1), 2));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 1), 5));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 3));
         mostraClient(demo);
     }
     
