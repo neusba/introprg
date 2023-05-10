cdc1f9e70a5a77fe76a958231110074ba165ff91
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2023-05-10 15:32:40 +0200

    Extracció de mètode en el càlcul de bonificacions del mètode Client.informe()

---
+++ 52_20_extraccio/Client.java
@@ -0,0 +1,66 @@
+/* Classe Client d'exemple per la creació de classes del programa de Gestor de Lloguers
+ * Al client se li generà un informe amb el registre de tots els seus lloguers i els seus respectius imports i bonificiacions afegides.
+ */
+
+import java.util.ArrayList;
+
+public class Client {
+    private String nif;
+    private String nom;
+    private String telefon;
+    private ArrayList<Lloguer> lloguers;
+
+    public Client(String nif, String nom, String telefon) {
+        this.nif = nif;
+        this.nom = nom;
+        this.telefon = telefon;
+        this.lloguers = new ArrayList<Lloguer>();
+    }
+
+    public String getNif()     { return nif; }
+    public String getNom()     { return nom; }
+    public String getTelefon() { return telefon; }
+    public ArrayList<Lloguer> getLloguers() { return lloguers; }
+
+    public void setNif(String nif) { this.nif = nif; }
+    public void setNom(String nom) { this.nom = nom; }
+    public void setTelefon(String telefon) { this.telefon = telefon; }
+
+    public String informe() {
+        double total = 0;
+        int bonificacions = 0;
+        String resultat = "Informe de lloguers del client " +
+            getNom() +
+            " (" + getNif() + ")\n";
+        for (Lloguer lloguer: lloguers) {
+            bonificacions += bonificacionsDeLloguer(lloguer);
+            // composa els resultats d'aquest lloguer
+            resultat += "\t" +
+                lloguer.getVehicle().getMarca() +
+                " " +
+                lloguer.getVehicle().getModel() + ": " +
+                (quantitat(lloguer) * 30) + "€" + "\n";
+            total += quantitat(lloguer) * 30;
+        }
+
+        // afegeix informació final
+        resultat += "Import a pagar: " + total + "€\n" +
+            "Punts guanyats: " + bonificacions + "\n";
+        return resultat;
+    }
+    // ############### REMPLAÇAMENT DE VARIABLES PER CRIDA ###################
+    public double quantitat(Lloguer lloguer) {
+        double quantitat = lloguer.quantitat();
+        return quantitat;
+    }
+    // ############## REFACCIÓ BONIFICACIONS #################################
+    private int bonificacionsDeLloguer(Lloguer lloguer) {
+        int bonificacions = 1;
+        // afegeix bonificació per dos dies de lloguer de Luxe
+        if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+                lloguer.getDies()>1 ) {
+            bonificacions ++;
+        }
+        return bonificacions;
+    }
+}
