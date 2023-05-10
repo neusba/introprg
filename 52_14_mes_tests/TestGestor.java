d07e032121279598802f09240a1653e732f7afbf
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2023-05-10 12:23:38 +0200

    Afegits totes les proves unitàries necessàries pel mètode Client.informe()

--- 52_14_mes_tests/TestGestor.java
+++ 52_14_mes_tests/TestGestor.java
@@ -25,6 +25,137 @@ public class TestGestor {
 
         Assertions.assertEquals(esperat, demo.informe());
     }
+    // 1 lloguer BASIC per 1 dia
+    @Test
+    public void lloguerBasicUnDia() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 1));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 90.0€\nImport a pagar: 90.0€\nPunts guanyats: 1\n";
 
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer BASIC per 2 dies
+    @Test
+    public void lloguerBasicDosDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 2));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 90.0€\nImport a pagar: 90.0€\nPunts guanyats: 1\n";
 
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer BASIC per 3 dies
+    @Test
+    public void lloguerBasicTresDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 3));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 90.0€\nImport a pagar: 90.0€\nPunts guanyats: 1\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer BASIC per 5 dies
+    @Test
+    public void lloguerBasicCincDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 5));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 180.0€\nImport a pagar: 180.0€\nPunts guanyats: 1\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 2 lloguer BASIC per 1 dia
+    @Test
+    public void dosLloguersBasicUnDia() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 1));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 1), 1));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 90.0€\n\tTata Vista: 90.0€\nImport a pagar: 180.0€\nPunts guanyats: 2\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 2 lloguer BASIC per 3 dies
+    @Test
+    public void dosLloguersBasicTresDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 3));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 1), 3));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 90.0€\n\tTata Vista: 90.0€\nImport a pagar: 180.0€\nPunts guanyats: 2\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 2 lloguer BASIC per 5 dies
+    @Test
+    public void dosLloguersBasicCincDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 1), 5));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 1), 5));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 180.0€\n\tTata Vista: 180.0€\nImport a pagar: 360.0€\nPunts guanyats: 2\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer GENERAL per 1 dia
+    @Test
+    public void unLloguerGeneralUnDia() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 2), 1));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 120.0€\nImport a pagar: 120.0€\nPunts guanyats: 1\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer GENERAL per 2 dies
+    @Test
+    public void unLloguerGeneralDosDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 2), 2));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 120.0€\nImport a pagar: 120.0€\nPunts guanyats: 1\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer GENERAL per 3 dies
+    @Test
+    public void unLloguerGeneralTresDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 2), 3));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 195.0€\nImport a pagar: 195.0€\nPunts guanyats: 1\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 2 lloguers GENERAL per 4 dies
+    @Test
+    public void dosLloguersGeneralQuatreDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 2), 4));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 2), 4));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 270.0€\n\tTata Vista: 270.0€\nImport a pagar: 540.0€\nPunts guanyats: 2\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 1 lloguer LUXE per 3 dies
+    @Test
+    public void unLloguerLuxeTresDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 3), 3));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 540.0€\nImport a pagar: 540.0€\nPunts guanyats: 2\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 2 lloguers LUXE per 2 dies
+    @Test
+    public void dosLloguersLuxeDosDies() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 3), 2));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 3), 2));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 360.0€\n\tTata Vista: 360.0€\nImport a pagar: 720.0€\nPunts guanyats: 4\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
+    // 3 lloguers LUXE per 1 dies
+    @Test
+    public void tresLloguersLuxeUnDia() {
+        Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 3), 1));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 3), 1));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Seat", "600", 3), 1));
+        String esperat = "Informe de lloguers del client Eugènia Salinas Roig (51590695Q)\n\tFiat Uno: 180.0€\n\tTata Vista: 180.0€\n\tSeat 600: 180.0€\nImport a pagar: 540.0€\nPunts guanyats: 3\n";
+
+        Assertions.assertEquals(esperat, demo.informe());
+    }
 }
