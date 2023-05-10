b6f5b28cfa1714cf318b8d944f60ed7348e5876a
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2023-05-10 18:08:45 +0200

    Eliminada variable temporal total del mètode Client.informe() i nou mètode per calcular l'import total generat

---
+++ 52_23_elimina_total/Client.java
@@ -0,0 +1,62 @@
+/* Classe Client d'exemple per la creació de classes del programa de Gestor de Lloguers
+ * Al client se li generà un informe amb el registre de tots els seus lloguers i els seus respectius imports i bonificiacions afegides.
+ */
+
+import java.util.ArrayList;
+
+public class Client {
+    private String nif;
+    private String nom;
+    private String telefon;
+    private ArrayList<Lloguer> lloguers;
+
+    public Client(String nif, String nom, String telefon) {
+        this.nif = nif;
+        this.nom = nom;
+        this.telefon = telefon;
+        this.lloguers = new ArrayList<Lloguer>();
+    }
+
+    public String getNif()     { return nif; }
+    public String getNom()     { return nom; }
+    public String getTelefon() { return telefon; }
+    public ArrayList<Lloguer> getLloguers() { return lloguers; }
+
+    public void setNif(String nif) { this.nif = nif; }
+    public void setNom(String nom) { this.nom = nom; }
+    public void setTelefon(String telefon) { this.telefon = telefon; }
+
+    public String informe() {
+        int bonificacions = 0;
+        String resultat = "Informe de lloguers del client " +
+            getNom() +
+            " (" + getNif() + ")\n";
+        for (Lloguer lloguer: lloguers) {
+            bonificacions += lloguer.bonificacions();
+            // composa els resultats d'aquest lloguer
+            resultat += "\t" +
+                lloguer.getVehicle().getMarca() +
+                " " +
+                lloguer.getVehicle().getModel() + ": " +
+                (quantitat(lloguer) * 30) + "€" + "\n";
+        }
+        // afegeix informació final
+        resultat += "Import a pagar: " + importTotal() + "€\n" +
+            "Punts guanyats: " + bonificacions + "\n";
+        return resultat;
+    }
+    // ############### REMPLAÇAMENT DE VARIABLES PER CRIDA ###################
+    public double quantitat(Lloguer lloguer) {
+        double quantitat = lloguer.quantitat();
+        return quantitat;
+    }
+    // ############## ELIMINACIÓ DE VARIABLES ################################
+    // Variable total
+    private double importTotal() {
+        double total = 0;
+        for (Lloguer lloguer: lloguers) {
+            total += quantitat(lloguer) * 30;
+        }
+        return total;
+    }
+}
