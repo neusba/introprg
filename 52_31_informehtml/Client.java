1b0ec8877917d289e7171c250794482a4d99fd4d
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-05-11 15:48:50 +0200

    Creades proves unitàries per l'informe HTML i codi corresponent  ∾ prgtest autocommit 52_31 #1

--- 52_31_informehtml/Client.java
+++ 52_31_informehtml/Client.java
@@ -94,7 +94,7 @@ public class Client {
     public String composaDetallHTML() {
         Locale.setDefault(Locale.US);
         String doubleFormat;
-        String resultat = "<table>\n <tr>\n  <td><strong>Marca</strong></td>\n  <td><strong>Model</strong></td>\n  <td><strong>Import</strong></td>\n </tr>\n";
+        String resultat = "<table>\n  <tr>\n    <td><strong>Marca</strong></td>\n    <td><strong>Model</strong></td>\n    <td><strong>Import</strong></td>\n </tr>\n";
         for (Lloguer lloguer : lloguers) {
             resultat += String.format(" <tr><td>%s</td><td>%s</td><td>%s€</td></tr>\n", lloguer.getVehicle().getMarca(), lloguer.getVehicle().getModel(), String.format("%.1f", lloguer.calculQuantitatsPerLloguer() * EUROS_PER_UNITAT_DE_COST)); }
         resultat += "</table>\n";
