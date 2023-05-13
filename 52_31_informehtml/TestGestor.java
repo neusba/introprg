bf35dd0ab6ae8f1760901f97a2816d9405f91279
Neus Bravo Arias <cf22neus.bravo@iesjoandaustria.org>
2023-05-13 15:15:56 +0200

    Canvi al String esperat en el test informeHTMLDemo()

--- 52_31_informehtml/TestGestor.java
+++ 52_31_informehtml/TestGestor.java
@@ -196,7 +196,22 @@ public class TestGestor {
     @Test
     public void informeHTMLDemo() {
         Client demo = new Client("51590695Q", "Eugènia Salinas Roig", "93614214242");
-        String esperat = demo.informeHTML();
+        demo.getLloguers().add(new Lloguer(new Vehicle("Seat", "600", 1), 2)); 
+        demo.getLloguers().add(new Lloguer(new Vehicle("Tata", "Vista", 1), 5));
+        demo.getLloguers().add(new Lloguer(new Vehicle("Fiat", "Uno", 3), 3)); 
+        String esperat = "<p>Informe de lloguers del client <em>Eugènia Salinas Roig</em> (<strong>51590695Q</strong>)</p>\n" + 
+                        "<table>\n" +
+                        "  <tr>\n" +
+                        "    <td><strong>Marca</strong></td>\n" +
+                        "    <td><strong>Model</strong></td>\n" +
+                        "    <td><strong>Import</strong></td>\n" +
+                        "  </tr>\n" +
+                        "  <tr><td>Seat</td><td>600</td><td>90.0€</td></tr>\n" +
+                        "  <tr><td>Tata</td><td>Vista</td><td>180.0€</td></tr>\n" +
+                        "  <tr><td>Fiat</td><td>Uno</td><td>540.0€</td></tr>\n" +
+                        "</table>\n" +
+                        "<p>Import a pagar: <em>810.0€</em></p>\n" +
+                        "<p>Punts guanyats: <em>4</em></p>";
 
         Assertions.assertEquals(esperat, demo.informeHTML());
     }
