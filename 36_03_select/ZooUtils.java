85aa9516c79ef43035550218e10e951e780066ad
prgtest <cf22neus.bravo@iesjoandaustria.org>
2023-05-25 16:25:55 +0200

    Afegit metode que obté les categories pel nom que tenen a la base de dades  ∾ prgtest autocommit 36_03 #4

--- 36_03_select/ZooUtils.java
+++ 36_03_select/ZooUtils.java
@@ -10,13 +10,11 @@ import java.util.LinkedList;
 public class ZooUtils {
     // Mètode que mostra les categories existents a la base de dades actuals 
     public static void mostraCategories(List<Categoria> categories) {
-        int comptaCategories = 0;
         if (categories.size() == 0) {
             System.out.println("Cap categoria");
         } else {
-            System.out.println("Nombre de categories: " + comptaCategories);
+            System.out.println("Nombre de categories: " + categories.size());
             for (Categoria categoria : categories) {
-                comptaCategories += 1;
                 System.out.println(categoria);
             }
         }
